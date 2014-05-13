c9c31ab8543714aa57bdbec37bdf4c4be42bbfe7
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2014-05-13 21:50:06 +0200

    Created a BOARD_DESCRIPTION structure.

--- src/common/board.vhdl
+++ src/common/board.vhdl
@@ -34,19 +34,50 @@ library	PoC;
 use			PoC.my_config.all;
 use			PoC.utils.all;
 use			PoC.strings.all;
+use			PoC.net.all;
 
 
 package board is
---	-- FPGA / Chip vendor
---	-- ===========================================================================
---
-
 	-- Functions extracting board and PCB properties from "MY_BOARD"
 	-- which is declared in package "my_config".
 	-- ===========================================================================
 	function MY_DEVICE_STRING(BoardConfig : string := "None")				return string;
 
-end board;
+	-- 
+	-- ===========================================================================
+	TYPE T_BRD_ETHERNET_DESC IS RECORD
+		IPStyle										: T_IPSTYLE;
+		RS_DataInterface					: T_NET_ETH_RS_DATA_INTERFACE;
+		PHY_Device								: T_NET_ETH_PHY_DEVICE;
+		PHY_DeviceAddress					: T_NET_ETH_PHY_DEVICE_ADDRESS;
+		PHY_DataInterface					: T_NET_ETH_PHY_DATA_INTERFACE;
+		PHY_ManagementInterface		: T_NET_ETH_PHY_MANAGEMENT_INTERFACE;
+	END RECORD;
+
+	TYPE T_BOARD_DESCRIPTION IS RECORD
+		Ethernet		: T_BRD_ETHERNET_DESC;
+	
+	END RECORD;
+
+
+	function MY_BOARD_STRUCT(BoardConfig : string := "None") return T_BOARD_DESCRIPTION;
+
+
+	CONSTANT C_BOARD_ML505			: T_BOARD_DESCRIPTION		:= (
+		Ethernet => (
+			IPStyle										=> IPSTYLE_SOFT,
+			RS_DataInterface					=> NET_ETH_RS_DATA_INTERFACE_GMII,
+			PHY_Device								=> NET_ETH_PHY_DEVICE_MARVEL_88E1111,
+			PHY_DeviceAddress					=> x"07",
+			PHY_DataInterface					=> NET_ETH_PHY_DATA_INTERFACE_GMII,
+			PHY_ManagementInterface		=> NET_ETH_PHY_MANAGEMENT_INTERFACE_MDIO
+		)
+	);
+	
+	CONSTANT C_BOARD_ML605			: T_BOARD_DESCRIPTION		:= C_BOARD_ML505;
+	CONSTANT C_BOARD_KC705			: T_BOARD_DESCRIPTION		:= C_BOARD_ML505;
+	CONSTANT C_BOARD_VC707			: T_BOARD_DESCRIPTION		:= C_BOARD_ML505;
+end;
 
 
 package body board is
@@ -85,4 +116,37 @@ package body board is
 		end if;
 	end MY_DEVICE_STRING;
 
+	-- purpose: extract vendor from MY_DEVICE
+	function MY_BOARD_STRUCT(BoardConfig : string := "None") return T_BOARD_DESCRIPTION is
+		constant MY_BRD : string := ite((BoardConfig = "None"), MY_BOARD, BoardConfig);
+	begin
+		if str_equal(MY_BRD, "Custom") then
+			report "A custom board has no predefined MY_BOARD_STRUCT" severity failure;
+		else
+			case MY_BRD'length is
+--				when 3 =>
+--					case MY_BRD(1 to 3) is
+--						when "DE0" =>				return "EP3C------";
+--						when "DE4" =>				return "EP4S------";
+--						when "DE5" =>				return "EP5S------";
+--						when others =>			report "Unknown board name in MY_BOARD = " & MY_BRD & "." severity failure;
+--					end case;
+				when 5 =>
+					case MY_BRD(1 to 5) is
+						when "ML505" =>			return C_BOARD_ML505;
+						when "ML605" =>			return C_BOARD_ML605;
+						when "KC705" =>			return C_BOARD_KC705;
+						when "VC707" =>			return C_BOARD_VC707;
+						when others =>			report "Unknown board name in MY_BOARD = " & MY_BRD & "." severity failure;
+					end case;
+--				when 8 =>
+--					case MY_BRD(1 to 8) is
+--						when "S2GXAVDK" =>	return "EP2S------";
+--						when others =>			report "Unknown board name in MY_BOARD = " & MY_BRD & "." severity failure;
+--					end case;
+				when others => 		 			report "Unknown board name in MY_BOARD = " & MY_BRD & "." severity failure;
+														 -- return statement is explicitly missing otherwise XST won't stop
+			end case;
+		end if;
+	end MY_BOARD_STRUCT;
 end board;
