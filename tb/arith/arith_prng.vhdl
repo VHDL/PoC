007d22acc6aa36ca1ce2485f79364e291afaf712
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2014-05-20 22:07:48 +0200

    Added file header to arith_prng testbench.

--- tb/arith/arith_prng.vhdl
+++ tb/arith/arith_prng.vhdl
@@ -1,3 +1,36 @@
+-- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+-- vim: tabstop=2:shiftwidth=2:noexpandtab
+-- kate: tab-width 2; replace-tabs off; indent-width 2;
+-- 
+-- ============================================================================================================================================================
+-- Testbench:				Pseudo-Random Number Generator (PRNG).
+-- 
+-- Authors:					Patrick Lehmann
+-- 
+-- Description:
+-- ------------------------------------
+--		Automated testbench for PoC.arith_prng
+--		The Pseudo-Random Number Generator is instanziated for 8 bits. The
+--		output sequence is compared to 256 precalculated values.
+--
+-- License:
+-- ============================================================================================================================================================
+-- Copyright 2007-2014 Technische Universitaet Dresden - Germany
+--										 Chair for VLSI-Design, Diagnostics and Architecture
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--		http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- ============================================================================================================================================================
+
 LIBRARY IEEE;
 USE			IEEE.STD_LOGIC_1164.ALL;
 USE			IEEE.NUMERIC_STD.ALL;
@@ -6,7 +39,6 @@ LIBRARY PoC;
 USE			PoC.utils.ALL;
 USE			PoC.vectors.ALL;
 USE			PoC.strings.ALL;
---USE			PoC.simulation.ALL;
 
 LIBRARY L_Testbench;
 
