12de24ae11213caadf8819fc047bca99488f40f9
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2014-06-24 02:37:30 +0200

    Added PowerShell wrapper scripts for Windows.

---
+++ poc.ps1
@@ -0,0 +1,53 @@
+# EMACS settings: -*-	tab-width: 2; indent-tabs-mode: t -*-
+# vim: tabstop=2:shiftwidth=2:noexpandtab
+# kate: tab-width 2; replace-tabs off; indent-width 2;
+# 
+# ==============================================================================
+#	PowerShell Script:	Wrapper Script to execute <PoC-Root>/py/Configuration.py
+# 
+#	Authors:						Patrick Lehmann
+# 
+# Description:
+# ------------------------------------
+#	This is a PowerShell wrapper script (executable) which:
+#		- saves the current working directory as an environment variable
+#		- delegates the call to <PoC-Root>/py/Wrapper.ps1
+#		-
+#
+# License:
+# ==============================================================================
+# Copyright 2007-2014 Technische Universitaet Dresden - Germany
+#											Chair for VLSI-Design, Diagnostics and Architecture
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#		http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# configure wrapper here
+$PoC_RootDir_RelPath =			"."
+$PoC_PyWrapper_Script =			"Configuration.py"
+$PoC_PyWrapper_MinVersion =	"3.4.0"
+
+# save parameters and current working directory
+$PoC_PyWrapper_Paramters = $args
+$PoC_PyWrapper_ScriptDir = Get-Location
+
+# search parameters for specific options like '-D' to enable batch script debug mode
+$PoC_PyWrapper_Debug =						$false
+$PoC_PyWrapper_LoadEnv_ISE =			$false
+$PoC_PyWrapper_LoadEnv_Vivado = 	$false
+
+foreach ($i in $args) {
+	$PoC_PyWrapper_Debug =					$PoC_PyWrapper_Debug -or ($i -clike "-*D*")
+}
+
+. ($PoC_RootDir_RelPath + "\py\Wrapper.ps1")
