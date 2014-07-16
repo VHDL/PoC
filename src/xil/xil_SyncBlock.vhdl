d9a40c86b05d1cdc9a0dd8209b410e12102bbe24
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2014-07-16 19:10:10 +0200

    Modified xil_SyncBlock to support multiple bits.

--- src/xil/xil_SyncBlock.vhdl
+++ src/xil/xil_SyncBlock.vhdl
@@ -45,46 +45,54 @@ USE			UNISIM.VCOMPONENTS.ALL;
 -- ============================================================================
 
 ENTITY xil_SyncBlock IS
+	GENERIC (
+		BITS					: POSITIVE																		:= 1		-- number of BITS to synchronize
+	);
 	PORT (
-		Clock					: IN	STD_LOGIC;					-- Clock to be synchronized to
-		DataIn				: IN	STD_LOGIC;					-- Data to be synchronized
-		DataOut				: OUT	STD_LOGIC						-- synchronised data
+		Clock					: IN	STD_LOGIC;																			-- Clock to be synchronized to
+		DataIn				: IN	STD_LOGIC_VECTOR(BITS - 1 DOWNTO 0);						-- Data to be synchronized
+		DataOut				: OUT	STD_LOGIC_VECTOR(BITS - 1 DOWNTO 0)							-- synchronised data
 	);
 END;
 
 
 ARCHITECTURE rtl OF xil_SyncBlock IS
-	SIGNAL DataSync_async				: STD_LOGIC;
-
-	-- Mark register "DataSync_async" as asynchronous
 	ATTRIBUTE ASYNC_REG												: STRING;
-	ATTRIBUTE ASYNC_REG			OF DataSync_async	: SIGNAL IS "TRUE";
-
-	-- Prevent XST from translating two FFs into SRL plus FF
 	ATTRIBUTE SHREG_EXTRACT										: STRING;
-	ATTRIBUTE SHREG_EXTRACT OF DataSync_async	: SIGNAL IS "NO";
-	ATTRIBUTE SHREG_EXTRACT OF DataOut				: SIGNAL IS "NO";
 
 BEGIN
 
-	FF1 : FD
-		GENERIC MAP (
-			INIT		=> '0'
-		)
-		PORT MAP (
-			C				=> Clock,
-			D				=> DataIn,
-			Q				=> DataSync_async
-	);
+	gen : FOR I IN 0 TO BITS - 1 GENERATE
+		SIGNAL DataSync_async				: STD_LOGIC;
+		SIGNAL DataSync_out					: STD_LOGIC;
+	
+		-- Mark register "DataSync_async" as asynchronous
+		ATTRIBUTE ASYNC_REG			OF DataSync_async	: SIGNAL IS "TRUE";
 
-	FF2 : FD
-		GENERIC MAP (
-			INIT		=> '0'
-		)
-		PORT MAP (
-			C				=> Clock,
-			D				=> DataSync_async,
-			Q				=> DataOut
-	);
+		-- Prevent XST from translating two FFs into SRL plus FF
+		ATTRIBUTE SHREG_EXTRACT OF DataSync_async	: SIGNAL IS "NO";
+		ATTRIBUTE SHREG_EXTRACT OF DataSync_out		: SIGNAL IS "NO";
+	BEGIN
+		FF1 : FD
+			GENERIC MAP (
+				INIT		=> '0'
+			)
+			PORT MAP (
+				C				=> Clock,
+				D				=> DataIn(I),
+				Q				=> DataSync_async
+		);
 
-	END;
+		FF2 : FD
+			GENERIC MAP (
+				INIT		=> '0'
+			)
+			PORT MAP (
+				C				=> Clock,
+				D				=> DataSync_async,
+				Q				=> DataSync_out
+		);
+		
+		DataOut(I)	<= DataSync_out;
+	END GENERATE;
+END ARCHITECTURE;
