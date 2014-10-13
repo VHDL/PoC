6b98ffaa415cc8f6077e77ee6c5eb2af4ee5eb38
steve <steffen.koehler@tu-dresden.de>
2014-10-13 15:17:04 +0200

    simplified SpeedControl

--- src/sata/sata_Physical_SpeedControl.vhdl
+++ src/sata/sata_Physical_SpeedControl.vhdl
@@ -47,40 +47,40 @@ USE			PoC.satadbg.ALL;
 
 ENTITY sata_Physical_SpeedControl IS
 	GENERIC (
-		DEBUG											: BOOLEAN							:= FALSE;												-- generate additional debug signals and preserve them (attribute keep)
-		ENABLE_DEBUGPORT					: BOOLEAN							:= FALSE;												-- enables the assignment of signals to the debugport
-		INITIAL_SATA_GENERATION		: T_SATA_GENERATION		:= C_SATA_GENERATION_MAX;
-		GENERATION_CHANGE_COUNT		: INTEGER							:= 32;
-		ATTEMPTS_PER_GENERATION		: INTEGER							:= 8
+		DEBUG				: BOOLEAN		:= FALSE;								-- generate additional debug signals and preserve them (attribute keep)
+		ENABLE_DEBUGPORT		: BOOLEAN		:= FALSE;								-- enables the assignment of signals to the debugport
+		INITIAL_SATA_GENERATION		: T_SATA_GENERATION	:= C_SATA_GENERATION_MAX;
+		GENERATION_CHANGE_COUNT		: INTEGER		:= 32;
+		ATTEMPTS_PER_GENERATION		: INTEGER		:= 8
 	);
 	PORT (
-		Clock											: IN	STD_LOGIC;
-		ClockEnable								: IN	STD_LOGIC;
-		Reset											: IN	STD_LOGIC;
+		Clock				: IN	STD_LOGIC;
+		ClockEnable			: IN	STD_LOGIC;
+		Reset				: IN	STD_LOGIC;
 
-		Command										: IN	T_SATA_PHY_SPEED_COMMAND;
-		Status										: OUT	T_SATA_PHY_SPEED_STATUS;
-		SATAGenerationMin					: IN	T_SATA_GENERATION;									-- 
-		SATAGenerationMax					: IN	T_SATA_GENERATION;									-- 
+		Command				: IN	T_SATA_PHY_SPEED_COMMAND;
+		Status				: OUT	T_SATA_PHY_SPEED_STATUS;
+		SATAGenerationMin		: IN	T_SATA_GENERATION;									-- 
+		SATAGenerationMax		: IN	T_SATA_GENERATION;									-- 
 
-		DebugPortOut							: OUT	T_SATADBG_PHYSICAL_SPEEDCONTROL_OUT;
+		DebugPortOut			: OUT	T_SATADBG_PHYSICAL_SPEEDCONTROL_OUT;
 
-		OOBC_Timeout							: IN	STD_LOGIC;
-		OOBC_Retry								: OUT	STD_LOGIC;
+		OOBC_Timeout			: IN	STD_LOGIC;
+		OOBC_Retry			: OUT	STD_LOGIC;
 		
 		-- reconfiguration interface
-		Trans_RP_Reconfig					: OUT	STD_LOGIC;
+		Trans_RP_Reconfig		: OUT	STD_LOGIC;
 		Trans_RP_SATAGeneration		: OUT	T_SATA_GENERATION;									-- 
 		Trans_RP_ReconfigComplete	: IN	STD_LOGIC;
 		Trans_RP_ConfigReloaded		: IN	STD_LOGIC;
-		Trans_RP_Lock							: OUT	STD_LOGIC;
-		Trans_RP_Locked						: IN	STD_LOGIC
+		Trans_RP_Lock			: OUT	STD_LOGIC;
+		Trans_RP_Locked			: IN	STD_LOGIC
 	);
 END;
 
 
 ARCHITECTURE rtl OF sata_Physical_SpeedControl IS
-	ATTRIBUTE KEEP					: BOOLEAN;
+	ATTRIBUTE KEEP		: BOOLEAN;
 	ATTRIBUTE FSM_ENCODING	: STRING;
 
 	TYPE T_SGEN_SGEN	IS ARRAY (T_SATA_GENERATION) OF T_SATA_GENERATION;
@@ -89,7 +89,7 @@ ARCHITECTURE rtl OF sata_Physical_SpeedControl IS
 
 	FUNCTION StartGen RETURN T_SGEN2_SGEN IS
 		CONSTANT ERROR_VALUE	: T_SATA_GENERATION	:= ite(SIMULATION, SATA_GENERATION_ERROR, SATA_GENERATION_1);
-		VARIABLE SG						: T_SGEN2_SGEN			:= (OTHERS => (OTHERS => ERROR_VALUE));
+		VARIABLE SG		: T_SGEN2_SGEN		:= (OTHERS => (OTHERS => ERROR_VALUE));
 	BEGIN
 		-- minimal			/	maximal gen.		==>	cmp value
 		-- ========================================================================
@@ -188,36 +188,37 @@ ARCHITECTURE rtl OF sata_Physical_SpeedControl IS
 		RETURN NG;
 	END;
 	
-	CONSTANT ROM_StartGeneration							: T_SGEN2_SGEN	:= StartGen;
-	CONSTANT ROM_NextGeneration 							: T_SGEN3_SGEN	:= NextGen;
+	CONSTANT ROM_StartGeneration	: T_SGEN2_SGEN	:= StartGen;
+	CONSTANT ROM_NextGeneration 	: T_SGEN3_SGEN	:= NextGen;
 
 	CONSTANT GENERATION_CHANGE_COUNTER_BITS		: POSITIVE			:= log2ceilnz(GENERATION_CHANGE_COUNT);
 	CONSTANT TRY_PER_GENERATION_COUNTER_BITS	: POSITIVE			:= log2ceilnz(ATTEMPTS_PER_GENERATION);
 
 	TYPE T_STATE IS (
-		ST_RESET,
-		ST_RECONFIG,		ST_RECONFIG_WAIT,
-		ST_NEGOTIATION,	ST_RETRY,
+		ST_WAIT,
+		ST_RECONFIG,
+		ST_RECONFIG_WAIT,
+		ST_RETRY,
 		ST_TIMEOUT,
-		ST_NEGOTIATION_ERROR
+		ST_ERROR
 	);
 	
 	-- Speed Negotiation - Statemachine
-	SIGNAL State												: T_STATE												:= ST_RESET;
-	SIGNAL NextState										: T_STATE;
-	ATTRIBUTE FSM_ENCODING	OF State		: SIGNAL IS getFSMEncoding_gray(DEBUG);
+	SIGNAL State			: T_STATE												:= ST_WAIT;
+	SIGNAL NextState		: T_STATE;
+	ATTRIBUTE FSM_ENCODING	OF State	: SIGNAL IS getFSMEncoding_gray(DEBUG);
 
-	SIGNAL Status_i											: T_SATA_PHY_SPEED_STATUS;
+	SIGNAL Status_i			: T_SATA_PHY_SPEED_STATUS;
 
-	SIGNAL SATAGeneration_rst						: STD_LOGIC;
-	SIGNAL SATAGeneration_Change				: STD_LOGIC;
-	SIGNAL SATAGeneration_Changed				: STD_LOGIC;
-	SIGNAL SATAGeneration_cur						: T_SATA_GENERATION							:= INITIAL_SATA_GENERATION;
-	SIGNAL SATAGeneration_nxt						: T_SATA_GENERATION;
+	SIGNAL SATAGeneration_rst	: STD_LOGIC;
+	SIGNAL SATAGeneration_Change	: STD_LOGIC;
+	SIGNAL SATAGeneration_Changed	: STD_LOGIC;
+	SIGNAL SATAGeneration_cur	: T_SATA_GENERATION							:= INITIAL_SATA_GENERATION;
+	SIGNAL SATAGeneration_nxt	: T_SATA_GENERATION;
 
-	SIGNAL OOBC_Retry_i									: STD_LOGIC;
-	SIGNAL Trans_RP_Reconfig_i					: STD_LOGIC;
-	SIGNAL Trans_RP_Lock_i							: STD_LOGIC;
+	SIGNAL OOBC_Retry_i		: STD_LOGIC;
+	SIGNAL Trans_RP_Reconfig_i	: STD_LOGIC;
+	SIGNAL Trans_RP_Lock_i		: STD_LOGIC;
 	
 	SIGNAL GenerationChange_Counter_rst	: STD_LOGIC;
 	SIGNAL GenerationChange_Counter_en	: STD_LOGIC;
@@ -246,7 +247,7 @@ BEGIN
 	PROCESS(SATAGeneration_rst, SATAGeneration_cur, SATAGeneration_Change, SATAGenerationMin, SATAGenerationMax)
 		VARIABLE SATAGeneration_nxt_v : T_SATA_GENERATION;
 	BEGIN
-		SATAGeneration_nxt				<= SATAGeneration_cur;
+		SATAGeneration_nxt		<= SATAGeneration_cur;
 		SATAGeneration_Changed		<= '0';
 		
 		IF (SATAGeneration_Change = '1') THEN
@@ -260,7 +261,7 @@ BEGIN
 			SATAGeneration_Changed	<= to_sl(SATAGeneration_cur /= SATAGeneration_nxt_v);
 			
 			-- assign new generation to *_nxt signal
-			SATAGeneration_nxt			<= SATAGeneration_nxt_v;
+			SATAGeneration_nxt	<= SATAGeneration_nxt_v;
 		END IF;
 	END PROCESS;
 
@@ -275,58 +276,34 @@ BEGIN
 	BEGIN
 		IF rising_edge(Clock) THEN
 			IF (Reset = '1') THEN
-				State	<= ST_RESET;
+				State	<= ST_WAIT;
 			ELSIF (ClockEnable = '1') THEN
 				State	<= NextState;
 			END IF;
 		END IF;
 	END PROCESS;
 
-	PROCESS(State,
-					Command,
-					OOBC_Timeout,
-					Trans_RP_ConfigReloaded,
-					SATAGeneration_Changed,
-					TryPerGeneration_Counter_ov, GenerationChange_Counter_ov)
+	PROCESS(State, Command, OOBC_Timeout, Trans_RP_ConfigReloaded,
+		SATAGeneration_Changed, TryPerGeneration_Counter_ov, GenerationChange_Counter_ov)
 	BEGIN
-		NextState														<= State;
+		NextState					<= State;
 		
-		Status_i														<= SATA_PHY_SPEED_STATUS_RESET;
+		Status_i					<= SATA_PHY_SPEED_STATUS_WAITING;
 		
-		SATAGeneration_rst									<= '0';
-		SATAGeneration_Change								<= '0';
-		OOBC_Retry_i												<= '0';
-		Trans_RP_Reconfig_i									<= '0';
-		Trans_RP_Lock_i											<= '1';
+		SATAGeneration_rst				<= '0';
+		SATAGeneration_Change				<= '0';
+		OOBC_Retry_i					<= '0';
+		Trans_RP_Reconfig_i				<= '0';
+		Trans_RP_Lock_i					<= '1';
 		
-		TryPerGeneration_Counter_rst				<= '0';
-		TryPerGeneration_Counter_en					<= '0';
-		GenerationChange_Counter_rst				<= '0';
-		GenerationChange_Counter_en					<= '0';
+		TryPerGeneration_Counter_rst			<= '0';
+		TryPerGeneration_Counter_en			<= '0';
+		GenerationChange_Counter_rst			<= '0';
+		GenerationChange_Counter_en			<= '0';
 	
 		CASE State IS
-			WHEN ST_RESET =>
-				Status_i												<= SATA_PHY_SPEED_STATUS_RESET;
-				
-				IF (Command = SATA_PHY_SPEED_CMD_RESET) OR (Command = SATA_PHY_SPEED_CMD_NEWLINK_UP) THEN
-					SATAGeneration_Change		<= '1';
-					SATAGeneration_rst		<= '1';
-					TryPerGeneration_Counter_rst	<= '1';
-					GenerationChange_Counter_rst	<= '1';
-					IF (SATAGeneration_Changed = '1') THEN
-						NextState	<= ST_RECONFIG;
-					ELSE
-						NextState	<= ST_RETRY;
-					END IF;
-				END IF;
-			
-			WHEN ST_RETRY =>
-				Status_i												<= SATA_PHY_SPEED_STATUS_NEGOTIATING;
-				OOBC_Retry_i										<= '1';
-				NextState												<= ST_NEGOTIATION;
-			
-			WHEN ST_NEGOTIATION =>
-				Status_i												<= SATA_PHY_SPEED_STATUS_NEGOTIATING;
+			WHEN ST_WAIT =>
+				Status_i												<= SATA_PHY_SPEED_STATUS_WAITING;
 
 				IF (Command = SATA_PHY_SPEED_CMD_RESET) OR (Command = SATA_PHY_SPEED_CMD_NEWLINK_UP) THEN
 					SATAGeneration_Change		<= '1';
@@ -342,12 +319,17 @@ BEGIN
 					NextState	<= ST_TIMEOUT;
 				END IF;
 			
+			WHEN ST_RETRY =>
+				Status_i												<= SATA_PHY_SPEED_STATUS_WAITING;
+				OOBC_Retry_i										<= '1';
+				NextState												<= ST_WAIT;
+			
 			WHEN ST_TIMEOUT =>
-				Status_i												<= SATA_PHY_SPEED_STATUS_NEGOTIATING;
+				Status_i												<= SATA_PHY_SPEED_STATUS_WAITING;
 				
 				IF (TryPerGeneration_Counter_ov = '1') THEN
 					IF (GenerationChange_Counter_ov = '1') THEN
-						NextState										<= ST_NEGOTIATION_ERROR;
+						NextState										<= ST_ERROR;
 					ELSE																					-- generation change counter allows => generation change
 						SATAGeneration_Change				<= '1';
 						TryPerGeneration_Counter_rst	<= '1';
@@ -360,27 +342,27 @@ BEGIN
 						END IF;
 					END IF;
 				ELSE																						-- tries per generation counter allows an other try at current generation
-					TryPerGeneration_Counter_en		<= '1';
-					NextState											<= ST_RETRY;
+					TryPerGeneration_Counter_en	<= '1';
+					NextState	<= ST_RETRY;
 				END IF;
 
 			WHEN ST_RECONFIG =>
-				Status_i												<= SATA_PHY_SPEED_STATUS_RECONFIGURATING;
-				Trans_RP_Lock_i									<= '0';
-				Trans_RP_Reconfig_i							<= '1';
-				NextState												<= ST_RECONFIG_WAIT;
+				Status_i	<= SATA_PHY_SPEED_STATUS_RECONFIGURATING;
+				Trans_RP_Lock_i		<= '0';
+				Trans_RP_Reconfig_i	<= '1';
+				NextState	<= ST_RECONFIG_WAIT;
 
 			WHEN ST_RECONFIG_WAIT =>
-				Status_i												<= SATA_PHY_SPEED_STATUS_RECONFIGURATING;
-				Trans_RP_Lock_i									<= '0';
+				Status_i	<= SATA_PHY_SPEED_STATUS_RECONFIGURATING;
+				Trans_RP_Lock_i		<= '0';
 				
 				IF (Trans_RP_ConfigReloaded = '1') THEN
 					NextState	<= ST_RETRY;
 				END IF;
 
-			WHEN ST_NEGOTIATION_ERROR =>
-				Trans_RP_Lock_i									<= '0';
-				Status_i												<= SATA_PHY_SPEED_STATUS_NEGOTIATION_ERROR;
+			WHEN ST_ERROR =>
+				Trans_RP_Lock_i		<= '0';
+				Status_i	<= SATA_PHY_SPEED_STATUS_NEGOTIATION_ERROR;
 
 				IF (Command = SATA_PHY_SPEED_CMD_RESET) OR (Command = SATA_PHY_SPEED_CMD_NEWLINK_UP) THEN
 					SATAGeneration_Change		<= '1';
@@ -397,10 +379,10 @@ BEGIN
 		END CASE;
 	END PROCESS;
 
-	Status						<= Status_i;
-	OOBC_Retry				<= OOBC_Retry_i;
+	Status			<= Status_i;
+	OOBC_Retry		<= OOBC_Retry_i;
 	Trans_RP_Reconfig	<= Trans_RP_Reconfig_i;
-	Trans_RP_Lock			<= Trans_RP_Lock_i;
+	Trans_RP_Lock		<= Trans_RP_Lock_i;
 
 	-- ================================================================
 	-- try counters
