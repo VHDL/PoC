9836d08dc8cb3afe0bebd3fc1a862c79bcff4339
Thomas B. Preußer <thomas.preusser@tu-dresden.de>
2014-12-18 11:05:29 +0100

    Renamed source to match package name. Corrected trailing whitespace errors.

--- src/common/txtio.vhdl
+++ src/common/txtio.vhdl
@@ -1,7 +1,7 @@
 -- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
 -- vim: tabstop=2:shiftwidth=2:noexpandtab
 -- kate: tab-width 2; replace-tabs off; indent-width 2;
--- 
+--
 -- ============================================================================
 -- Package:     IO-related functions.
 --
@@ -23,13 +23,13 @@
 -- ============================================================================
 -- Copyright 2007-2014 Technische Universitaet Dresden - Germany,
 --  					 				 Chair for VLSI-Design, Diagnostics and Architecture
--- 
+--
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
--- 
+--
 --		http://www.apache.org/licenses/LICENSE-2.0
--- 
+--
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -81,5 +81,4 @@ package body txtio is
 		writeline(stderr_line);
 	end procedure;
 	
-end package body; 
-
+end package body;
