1e60094a03a17a95ae0e2d6113515588f448ebe1
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2015-07-13 22:37:10 +0200

    Fixed testbench files for PoC.fifo.cc_got.

--- tb/fifo/fifo_cc_got_tb.vhdl
+++ tb/fifo/fifo_cc_got_tb.vhdl
@@ -1,3 +1,36 @@
+-- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+-- vim: tabstop=2:shiftwidth=2:noexpandtab
+-- kate: tab-width 2; replace-tabs off; indent-width 2;
+-- 
+-- ============================================================================
+-- Authors:					Thomas B. Preusser
+--
+-- Testbench:				Testbench for a FIFO with Common Clock (cc) and Pipelined Interface
+--
+-- Description:
+-- ------------------------------------
+--		TODO
+--		
+--
+-- License:
+-- ============================================================================
+-- Copyright 2007-2015 Technische Universitaet Dresden - Germany,
+--										 Chair for VLSI-Design, Diagnostics and Architecture
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--		http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- ============================================================================
+
+
 entity fifo_cc_got_tb is
 end entity;
 
@@ -37,12 +70,14 @@ begin
     constant OUTPUT_REG : boolean := CFG_CASE(2) = '1';
     
     -- Local Component Ports
-    signal put   : std_logic;
-    signal din   : std_logic_vector(D_BITS-1 downto 0);
-    signal full  : std_logic;
-    signal got   : std_logic;
-    signal dout  : std_logic_vector(D_BITS-1 downto 0);
-    signal valid : std_logic;
+    signal put				: std_logic;
+    signal din				: std_logic_vector(D_BITS-1 downto 0);
+    signal full				: std_logic;
+		signal estate_wr	: std_logic_vector(ESTATE_WR_BITS - 1 downto 0);
+    signal got				: std_logic;
+    signal dout				: std_logic_vector(D_BITS-1 downto 0);
+    signal valid			: std_logic;
+		signal fstate_rd	: std_logic_vector(FSTATE_RD_BITS - 1 downto 0);
 
   begin
 
@@ -62,11 +97,11 @@ begin
         put       => put,
         din       => din,
         full      => full,
-        estate_wr => open,
+        estate_wr => estate_wr,
         got       => got,
         dout      => dout,
         valid     => valid,
-        fstate_rd => open
+        fstate_rd => fstate_rd
       );
 
     -- Writer
