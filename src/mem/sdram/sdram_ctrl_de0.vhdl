1c88f287615dda53ed72eca31558b4ecc890f9d2
Martin Zabel <martin.zabel@tu-dresden.de>
2015-11-13 14:16:05 +0100

    Ported from old CVS. Tested with DE0 memory testbench.

--- src/mem/sdram/sdram_ctrl_de0.vhdl
+++ src/mem/sdram/sdram_ctrl_de0.vhdl
@@ -1,20 +1,15 @@
---
--- Copyright (c) 2013
--- Technische Universitaet Dresden, Dresden, Germany
--- Faculty of Computer Science
--- Institute for Computer Engineering
--- Chair for VLSI-Design, Diagnostics and Architecture
+-- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+-- vim: tabstop=2:shiftwidth=2:noexpandtab
+-- kate: tab-width 2; replace-tabs off; indent-width 2;
 -- 
--- For internal educational use only.
--- The distribution of source code or generated files
--- is prohibited.
---
-
---
--- Entity: sdram_ctrl_de0
--- Author(s): Martin Zabel
+-- ============================================================================
+-- Authors:					Martin Zabel
 -- 
--- Controller for ISSI SDR-SDRAM for Altera DE0 Board
+-- Module:					Controller for ISSI SDR-SDRAM for Altera DE0 Board
+--
+-- Description:
+-- ------------------------------------
+-- Complete controller for ISSI SDR-SDRAM for Altera DE0 Board.
 -- SDRAM Device: IS42S16400F
 --
 -- CLK_PERIOD = clock period in nano seconds. All SDRAM timings are
@@ -35,9 +30,24 @@
 --
 -- Synchronous resets are used.
 --
--- Revision:    $Revision: 1.5 $
--- Last change: $Date: 2013-06-11 17:06:07 $
---
+-- License:
+-- ============================================================================
+-- Copyright 2007-2015 Technische Universitaet Dresden - Germany,
+--										 Chair for VLSI-Design, Diagnostics and Architecture
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--		http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- ============================================================================
+
 -------------------------------------------------------------------------------
 -- Naming Conventions:
 -- (Based on: Keating and Bricaud: "Reuse Methodology Manual")
