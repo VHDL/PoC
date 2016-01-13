05b45c38a490e9a4d89584a955fb9516626a91b8
Martin Zabel <martin.zabel@tu-dresden.de>
2016-01-13 13:28:03 +0100

    Fixed str_format, tested with uart_fifo and sata_StreamingStack.

--- src/common/physical.vhdl
+++ src/common/physical.vhdl
@@ -4,6 +4,7 @@
 -- 
 -- ============================================================================
 -- Authors:					Patrick Lehmann
+-- 									Martin Zabel
 -- 
 -- Package:					This VHDL package declares new physical types and their
 --									conversion functions.
