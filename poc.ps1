a39819e9cd4a39f515a144676b563128970563bd
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-07-26 01:53:27 +0200

    Wrapper.ps1 is no longer needed.

--- poc.ps1
+++ poc.ps1
@@ -32,35 +32,67 @@
 # ==============================================================================
 #
 # Change this, if PoC solutions and PoC projects are used
-$RootDir_RelPath =			"."		# relative path to PoC root directory
+$PoC_RelPath =					"."		# relative path to PoC root directory
 $PoC_Solution =					""		# solution name
 
 # save parameters and current working directory
-$PyWrapper_Parameters =	$args
 $PyWrapper_WorkingDir =	Get-Location
-$PyWrapper_ExitCode =		0
 
 # Configure PoC environment here
 $PoC_PythonDir =				"py"
+$PoC_ScriptPy =					"$PoC_PythonDir\PoC.py"
 $PoC_WrapperDir =				"py\Wrapper"
 $PoC_Module =						"PoC"
 $PoC_Wrapper =					"Wrapper.ps1"
 
-# Configure wrapper here
-$Py_MinVersion =				"3.5"
-$PoC_ScriptPy =					"$PoC_PythonDir\PoC.py"
+# load PoC module
+$PoCRootDir =						Convert-Path (Resolve-Path ($PSScriptRoot + "\" + $PoC_RelPath))
+Import-Module "$PoCRootDir\$PoC_WrapperDir\$PoC_Module.psm1" -ArgumentList @($PoCRootDir)
 
-$PoCRootDir =						Convert-Path (Resolve-Path ($PSScriptRoot + "\" + $RootDir_RelPath))
-Import-Module "$PoCRootDir\$PoC_WrapperDir\$PoC_Module.psm1" -ArgumentList @($Py_MinVersion, $PoCRootDir)
+# scan script parameters and mark environment to be loaded
+$Debug, $PyWrapper_LoadEnv = Get-PoCEnvironmentArray $args
+# execute vendor and tool pre-hook files if present
+Invoke-OpenEnvironment $PyWrapper_LoadEnv | Out-Null
 
-# invoke main wrapper
-. "$PoCRootDir\$PoC_WrapperDir\$PoC_Wrapper"
+# print debug messages
+if ($Debug -eq $true ) {
+	Write-Host "This is the PoC-Library script wrapper operating in debug mode." -ForegroundColor Yellow
+	Write-Host ""
+	Write-Host "Directories:" -ForegroundColor Yellow
+	Write-Host "  PoC Root        $PoC_RootDir" -ForegroundColor Yellow
+	Write-Host "  Working         $PyWrapper_WorkingDir" -ForegroundColor Yellow
+	Write-Host "Script:" -ForegroundColor Yellow
+	Write-Host "  Filename        $PoC_ScriptPy" -ForegroundColor Yellow
+	Write-Host "  Solution        $PoC_Solution" -ForegroundColor Yellow
+	Write-Host "  Parameters      $args" -ForegroundColor Yellow
+	Write-Host "Load Environment:" -ForegroundColor Yellow
+	Write-Host "  Lattice Diamond $($PyWrapper_LoadEnv['Lattice']['Tools']['Diamond']['Load'])"	-ForegroundColor Yellow
+	Write-Host "  Xilinx ISE      $($PyWrapper_LoadEnv['Xilinx']['Tools']['ISE']['Load'])"			-ForegroundColor Yellow
+	Write-Host "  Xilinx Vivado   $($PyWrapper_LoadEnv['Xilinx']['Tools']['Vivado']['Load'])"		-ForegroundColor Yellow
+	Write-Host ""
+}
 
-# restore working directory if changed
-Set-Location $PyWrapper_WorkingDir
+# execute script with appropriate Python interpreter and all given parameters
+if ($PoC_Solution -eq "")
+{	$Command = "$Python_Interpreter $Python_Parameters $PoC_RootDir\$PoC_ScriptPy $args"													}
+else
+{	$Command = "$Python_Interpreter $Python_Parameters $PoC_RootDir\$PoC_ScriptPy --sln=$PoC_Solution $args"			}
+	
+# execute script with appropriate Python interpreter and all given parameters
+if ($Debug -eq $true)	{	Write-Host "launching: '$Command'" -ForegroundColor Yellow	}
+Invoke-Expression $Command
+$PyWrapper_ExitCode = $LastExitCode
+
+
+Invoke-CloseEnvironment $PyWrapper_LoadEnv | Out-Null
 
 # unload PowerShell module
 Remove-Module $PoC_Module
+# clean up environment variables
+$env:PoCRootDirectory =			$null
+
+# restore working directory if changed
+Set-Location $PyWrapper_WorkingDir
 
 # return exit status
 exit $PyWrapper_ExitCode
