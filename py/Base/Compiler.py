f4f9ff41b1b574e8a8d287742a6885dbed1718e4
Martin Zabel <martin.zabel@tu-dresden.de>
2016-08-03 15:50:25 +0200

    Python Infra: Hotfix for post-processing rules.

--- py/Base/Compiler.py
+++ py/Base/Compiler.py
@@ -1,30 +1,30 @@
 # EMACS settings: -*-	tab-width: 2; indent-tabs-mode: t; python-indent-offset: 2 -*-
 # vim: tabstop=2:shiftwidth=2:noexpandtab
 # kate: tab-width 2; replace-tabs off; indent-width 2;
-# 
+#
 # ==============================================================================
 # Authors:          Patrick Lehmann
 #                   Martin Zabel
-# 
+#
 # Python Class:     Base class for all PoC***Compilers
-# 
+#
 # Description:
 # ------------------------------------
 #		TODO:
-#		- 
-#		- 
+#		-
+#		-
 #
 # License:
 # ==============================================================================
 # Copyright 2007-2016 Technische Universitaet Dresden - Germany
 #                     Chair for VLSI-Design, Diagnostics and Architecture
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -323,7 +323,7 @@ class Compiler(Shared):
 					appendPattern =   self.Host.PoCConfig.Interpolation.interpolate(self.Host.PoCConfig, netlist.ConfigSectionName, "RulesFile", rule.AppendPattern, {})
 					task = AppendLineTask(Path(filePath), appendPattern)
 					postReplaceTasks.append(task)
-				elif isinstance(rule, CopyRuleMixIn):
+				elif isinstance(rule, (CopyRuleMixIn, DeleteRuleMixIn)):
 					pass
 				else:
 					raise CompilerException("Unknown post-process rule '{0!s}'.".format(rule))
