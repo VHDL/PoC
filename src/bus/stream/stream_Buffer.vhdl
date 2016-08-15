35f4fbc7f0bc42cb6e59592c9681e957c532a59b
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-08-16 01:23:36 +0200

    Fixed documentation files and embedded VHDL documentation.

--- src/bus/stream/stream_Buffer.vhdl
+++ src/bus/stream/stream_Buffer.vhdl
@@ -8,9 +8,10 @@
 --
 -- Description:
 -- -------------------------------------
--- This module implements a generic buffer (FIFO) for the PoC.Stream protocol.
--- It is generic in ``DATA_BITS`` and in ``META_BITS`` as well as in FIFO depths
--- for data and meta information.
+-- This module implements a generic buffer (FIFO) for the
+-- :doc:`PoC.Stream </References/Interfaces/Stream>` protocol. It is generic in
+-- ``DATA_BITS`` and in ``META_BITS`` as well as in FIFO depths for data and
+-- meta information.
 --
 -- License:
 -- =============================================================================
