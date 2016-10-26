dec5572532f115ae4839d5a67882f8c297d44b7e
Martin Zabel <martin.zabel@tu-dresden.de>
2016-10-26 10:58:49 +0200

    Updated documentation for ocram_sp. Added testbench for ocram_sp.

--- src/mem/ocram/ocram_sp.vhdl
+++ src/mem/ocram/ocram_sp.vhdl
@@ -9,21 +9,30 @@
 --
 -- Description:
 -- -------------------------------------
--- Inferring / instantiating enhanced single port memory, with:
+-- Inferring / instantiating single port memory, with:
 --
 -- * single clock, clock enable,
 -- * 1 read/write port.
 --
--- When writing data, the read output will be unknown which is aka. "don't
--- care behavior". The read output will be unknown for the full write-cycle
--- time, which starts at the rising-edge of the clock and (in the worst case)
--- extends until the next rising-edge of the clock.
+-- Command Truth Table:
 --
--- .. WARNING::
---    The simulated behavior on RT-level is too optimistic. During a
---    write, always the new data will be returned as read value.
+-- == == ================
+-- ce we Command
+-- == == ================
+-- 0  X  No operation
+-- 1  0  Read from memory
+-- 1  1  Write to memory
+-- == == ================
 --
--- .. TODO:: Implement correct behavior for RT-level simulation.
+-- Both reading and writing are synchronous to the rising-edge of the clock.
+-- Thus, when reading, the memory data will be outputted after the
+-- clock edge, i.e, in the following clock cycle.
+--
+-- When writing data, the read output will output the new data (in the
+-- following clock cycle) which is aka. "write-first behavior". This behavior
+-- also applies to Altera M20K memory blocks as described in the Altera:
+-- "Stratix 5 Device Handbook" (S5-5V1). The documentation in the Altera:
+-- "Embedded Memory User Guide" (UG-01068) is wrong.
 --
 -- License:
 -- =============================================================================
