f6c062cb86e324a10f2bda233c30451a9b6c7adc
Martin Zabel <martin.zabel@web.de>
2016-10-27 22:18:41 +0200

    Started implementation of simulation model for ocram_tdp.

---
+++ other/vsim/ocram_tdp_sim/wave.do
@@ -0,0 +1,36 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate /ocram_tdp_sim_tb/clk
+add wave -noupdate /ocram_tdp_sim_tb/ce1
+add wave -noupdate /ocram_tdp_sim_tb/ce2
+add wave -noupdate /ocram_tdp_sim_tb/we1
+add wave -noupdate /ocram_tdp_sim_tb/we2
+add wave -noupdate /ocram_tdp_sim_tb/a1
+add wave -noupdate /ocram_tdp_sim_tb/a2
+add wave -noupdate /ocram_tdp_sim_tb/d1
+add wave -noupdate /ocram_tdp_sim_tb/d2
+add wave -noupdate /ocram_tdp_sim_tb/q1
+add wave -noupdate /ocram_tdp_sim_tb/q2
+add wave -noupdate /ocram_tdp_sim_tb/rd_d1
+add wave -noupdate /ocram_tdp_sim_tb/rd_d2
+add wave -noupdate /ocram_tdp_sim_tb/exp_q1
+add wave -noupdate /ocram_tdp_sim_tb/exp_q2
+add wave -noupdate -divider UUT
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {19758 ps} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 150
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 1
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ps
+update
+WaveRestoreZoom {0 ps} {462 ns}
---
+++ tb/mem/ocram/ocram_tdp_sim.vhdl
@@ -0,0 +1,204 @@
+-- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+-- vim: tabstop=2:shiftwidth=2:noexpandtab
+-- kate: tab-width 2; replace-tabs off; indent-width 2;
+-- =============================================================================
+-- Authors:				 	Martin Zabel
+--
+-- Entity:				 	True dual-port memory.
+--
+-- Description:
+-- -------------------------------------
+-- Simulation model for true dual-port memory, with:
+--
+-- * dual clock, clock enable,
+-- * 2 read/write ports.
+--
+-- The interface matches that of the IP core PoC.mem.ocram.tdp.
+-- But the implementation there is restricted to the description supported by
+-- various synthesis compilers. The implementation here also simulates the
+-- correct Mixed-Port Read-During-Write Behavior and handles X propagation.
+--
+-- .. TODO::
+--    * Write collision check
+--    * Mixed-port read-during-write behavior
+--
+-- License:
+-- =============================================================================
+-- Copyright 2016-2016 Technische Universitaet Dresden - Germany
+--										 Chair for VLSI-Design, Diagnostics and Architecture
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--		http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- =============================================================================
+
+
+library	IEEE;
+use			IEEE.std_logic_1164.all;
+use			IEEE.numeric_std.all;
+
+library PoC;
+use			PoC.utils.all;
+use			PoC.strings.all;
+use			PoC.vectors.all;
+use			PoC.mem.all;
+
+
+entity ocram_tdp_sim is
+	generic (
+		A_BITS		: positive;															-- number of address bits
+		D_BITS		: positive;															-- number of data bits
+		FILENAME	: string		:= ""												-- file-name for RAM initialization
+	);
+	port (
+		clk1 : in	std_logic;															-- clock for 1st port
+		clk2 : in	std_logic;															-- clock for 2nd port
+		ce1	: in	std_logic;															-- clock-enable for 1st port
+		ce2	: in	std_logic;															-- clock-enable for 2nd port
+		we1	: in	std_logic;															-- write-enable for 1st port
+		we2	: in	std_logic;															-- write-enable for 2nd port
+		a1	 : in	unsigned(A_BITS-1 downto 0);						-- address for 1st port
+		a2	 : in	unsigned(A_BITS-1 downto 0);						-- address for 2nd port
+		d1	 : in	std_logic_vector(D_BITS-1 downto 0);		-- write-data for 1st port
+		d2	 : in	std_logic_vector(D_BITS-1 downto 0);		-- write-data for 2nd port
+		q1	 : out std_logic_vector(D_BITS-1 downto 0);		-- read-data from 1st port
+		q2	 : out std_logic_vector(D_BITS-1 downto 0) 		-- read-data from 2nd port
+	);
+end entity;
+
+
+architecture sim of ocram_tdp_sim is
+	constant DEPTH : positive := 2**A_BITS;
+		subtype word_t	is std_logic_vector(D_BITS - 1 downto 0);
+		type		ram_t		is array(0 to DEPTH - 1) of word_t;
+
+	impure function ocram_InitMemory(FilePath : string) return ram_t is
+		variable Memory		: T_SLM(DEPTH - 1 downto 0, word_t'range);
+		variable res			: ram_t;
+	begin
+		if str_length(FilePath) = 0 then
+			-- shortcut required by Vivado
+			return (others => (others => ite(SIMULATION, 'U', '0')));
+		elsif mem_FileExtension(FilePath) = "mem" then
+			Memory	:= mem_ReadMemoryFile(FilePath, DEPTH, word_t'length, MEM_FILEFORMAT_XILINX_MEM, MEM_CONTENT_HEX);
+		else
+			Memory	:= mem_ReadMemoryFile(FilePath, DEPTH, word_t'length, MEM_FILEFORMAT_INTEL_HEX, MEM_CONTENT_HEX);
+		end if;
+
+		for i in Memory'range(1) loop
+			for j in word_t'range loop
+				res(i)(j)		:= Memory(i, j);
+			end loop;
+		end loop;
+		return  res;
+	end function;
+
+	signal ram			: ram_t		:= ocram_InitMemory(FILENAME);
+
+	-- write to memory, 'X' means maybe write
+	signal write1 : X01;
+	signal write2 : X01;
+
+	-- read only from memory, 'X' means maybe read
+	signal read1  : X01;
+	signal read2  : X01;
+begin
+	assert SIMULATION report "This model is only for simulation." severity error;
+
+  write1 <= to_x01(ce1 and we1);
+  read1  <= to_x01(ce1 and not we1);
+  write2 <= to_x01(ce2 and we2);
+  read2  <= to_x01(ce2 and not we2);
+
+	process (clk1, clk2)
+	begin	-- process
+		if rising_edge(clk1) then
+			if write1 = '1' then
+				-- RAM is definitely written ...
+				if is_x(std_logic_vector(a1)) then
+					-- ... but address is unknown
+					ram <= (others => (others => 'X'));
+				else
+					--- ... and address is well known
+					ram(to_integer(a1)) <= d1;
+				end if;
+				-- same-port read during write: return new data
+				q1 <= d1;
+
+			elsif write1 = 'X' then
+				-- RAM may be written ...
+				if is_x(std_logic_vector(a1)) then
+					-- ... but address is unknown
+					ram <= (others => (others => 'X'));
+				else
+					--- ... and address is well known
+					ram(to_integer(a1)) <= (others => 'X');
+				end if;
+				-- same-port read during write: unknown data
+				q1 <= (others => 'X');
+			end if;
+
+			if read1 = '1' then
+				-- Definitely read only from RAM ...
+				if is_x(std_logic_vector(a1)) then
+					-- ... but address is unknown
+					q1 <= (others => 'X');
+				else
+					q1 <= ram(to_integer(a1));
+				end if;
+			elsif read1 = 'X' then
+				-- Maybe read only from RAM
+				q1 <= (others => 'X');
+			end if;
+		end if;
+
+		if rising_edge(clk2) then
+			if write2 = '1' then
+				-- RAM is definitely written ...
+				if is_x(std_logic_vector(a2)) then
+					-- ... but address is unknown
+					ram <= (others => (others => 'X'));
+				else
+					--- ... and address is well known
+					ram(to_integer(a2)) <= d2;
+				end if;
+				-- same-port read during write: return new data
+				q2 <= d2;
+
+			elsif write2 = 'X' then
+				-- RAM may be written ...
+				if is_x(std_logic_vector(a2)) then
+					-- ... but address is unknown
+					ram <= (others => (others => 'X'));
+				else
+					--- ... and address is well known
+					ram(to_integer(a2)) <= (others => 'X');
+				end if;
+				-- same-port read during write: unknown data
+				q1 <= (others => 'X');
+			end if;
+
+			if read2 = '1' then
+				-- Definitely read only from RAM ...
+				if is_x(std_logic_vector(a2)) then
+					-- ... but address is unknown
+					q2 <= (others => 'X');
+				else
+					q2 <= ram(to_integer(a2));
+				end if;
+			elsif read2 = 'X' then
+				-- Maybe read only from RAM
+				q2 <= (others => 'X');
+			end if;
+		end if;
+	end process;
+
+end architecture;
