2ade92cdc236db987d694d589e32a2b16c861c8a
Martin Zabel <martin.zabel@web.de>
2016-11-06 11:08:40 +0100

    Restructured cache_mem_tb, added more test patterns.

--- tb/cache/cache_mem_tb.vhdl
+++ tb/cache/cache_mem_tb.vhdl
@@ -10,13 +10,12 @@
 -- Description:
 -- ------------------------------------
 -- Test cache_mem using two memories. One connected behind the cache, and one
--- directly attached to the stimuli generator. The checker compares the
--- result of read requests to the cache with the result from the direct
--- attached memory.
+-- directly attached to the CPU. The CPU compares the result of read requests
+-- issued to the cache with the result from the direct attached memory.
 --
--- Stimuli / Checker  ---+--- Cache ---- 1st memory
---                       |
---                       +--- 2nd memory
+-- CPU  ---+--- Cache (UUT) ---- 1st memory
+--         |
+--         +--- 2nd memory
 --
 -- License:
 -- ============================================================================
@@ -57,21 +56,30 @@ architecture sim of cache_mem_tb is
   constant REPLACEMENT_POLICY : string   := "LRU";
   constant CACHE_LINES        : positive := 32;
   constant ASSOCIATIVITY      : positive := 4;
-  constant ADDR_BITS          : positive := 6;
-  constant BYTE_ADDR_BITS     : natural  := 0;
-  constant DATA_BITS          : positive := 8;
+  constant ADDR_BITS          : positive := 7;
+  constant BYTE_ADDR_BITS     : natural  := 1;
+  constant DATA_BITS          : positive := 16;
+	constant WORD_ADDR_BITS     : positive := ADDR_BITS-BYTE_ADDR_BITS;
+
+	-- Cache accesses are always aligned to a word boundary. A memory word and a
+	-- cache line consist of DATA_BITS bits. For example if DATA_BITS=16:
+	--
+	-- * word address 0 selects the bits  0..15 in memory, 
+	-- * word address 1 selects the bits 16..31 in memory, and so on.
 
 	-- Global signals
   signal clk : std_logic := '1';
   signal rst : std_logic;
 
-	-- Bus between Stimuli / Checker and Cache
-	-- Request signals are shared with 2nd Memory, issue request only if Cache is
-	-- ready. The 2nd memory is always ready after reset.
+	-- Request from CPU
+  signal cpu_req   : std_logic;
+  signal cpu_write : std_logic;
+  signal cpu_addr  : unsigned(ADDR_BITS-1 downto BYTE_ADDR_BITS);
+  signal cpu_wdata : std_logic_vector(DATA_BITS-1 downto 0);
+
+	-- Bus between CPU and Cache
+	-- write / addr / wdata are directly connected to the CPU
   signal cache_req   : std_logic;
-  signal cache_write : std_logic;
-  signal cache_addr  : unsigned(ADDR_BITS-1 downto BYTE_ADDR_BITS);
-  signal cache_wdata : std_logic_vector(DATA_BITS-1 downto 0);
   signal cache_rdy   : std_logic;
   signal cache_rstb  : std_logic;
   signal cache_rdata : std_logic_vector(DATA_BITS-1 downto 0);
@@ -85,8 +93,9 @@ architecture sim of cache_mem_tb is
   signal mem1_rstb  : std_logic;
   signal mem1_rdata : std_logic_vector(DATA_BITS-1 downto 0);
 
-	-- Bus between Stimuli / Checker and 2nd Memory
-	-- Request signals are shared with CPU side of Cache.
+	-- Bus between CPU and 2nd Memory
+	-- write / addr / wdata are directly connected to the CPU
+  signal mem2_req   : std_logic;
   signal mem2_rdy   : std_logic;
   signal mem2_rstb  : std_logic;
   signal mem2_rdata : std_logic_vector(DATA_BITS-1 downto 0);
@@ -95,7 +104,7 @@ architecture sim of cache_mem_tb is
 	signal wdata_got : std_logic;
 	signal wdata_val : std_logic_vector(DATA_BITS-1 downto 0);
 
-	-- Control signals between Stimuli and Checker
+	-- Control signals between Request Generator and Checker of CPU
 	signal finished : boolean := false;
 
 begin
@@ -105,7 +114,7 @@ begin
 	simGenerateClock(clk, CLOCK_FREQ);
 
 	-- The Cache
-	cache_inst: entity poc.cache_mem
+	UUT: entity poc.cache_mem
     generic map (
       REPLACEMENT_POLICY => REPLACEMENT_POLICY,
       CACHE_LINES        => CACHE_LINES,
@@ -117,9 +126,9 @@ begin
       clk       => clk,
       rst       => rst,
       cpu_req   => cache_req,
-      cpu_write => cache_write,
-      cpu_addr  => cache_addr,
-      cpu_wdata => cache_wdata,
+      cpu_write => cpu_write,
+      cpu_addr  => cpu_addr,
+      cpu_wdata => cpu_wdata,
       cpu_rdy   => cache_rdy,
       cpu_rstb  => cache_rstb,
       cpu_rdata => cache_rdata,
@@ -130,6 +139,9 @@ begin
       mem_rdy   => mem1_rdy,
       mem_rstb  => mem1_rstb,
       mem_rdata => mem1_rdata);
+
+	-- request only if also 2nd memory is ready
+	cache_req <= cpu_req and mem2_rdy;
 
 	-- The 1st Memory
 	memory1: entity work.mem_model
@@ -155,15 +167,18 @@ begin
 		port map (
 			clk       => clk,
 			rst       => rst,
-			mem_req   => cache_req,
-			mem_write => cache_write,
-			mem_addr  => cache_addr,
-			mem_wdata => cache_wdata,
+			mem_req   => mem2_req,
+			mem_write => cpu_write,
+			mem_addr  => cpu_addr,
+			mem_wdata => cpu_wdata,
 			mem_rdy   => mem2_rdy,
 			mem_rstb  => mem2_rstb,
 			mem_rdata => mem2_rdata);
 
-	-- The Write-Data Generator
+	-- request only if also cache is ready
+	mem2_req <= cpu_req and cache_rdy;
+
+	-- The Write-Data Generator of the CPU
 	wdata_prng: entity poc.arith_prng
     generic map (BITS => DATA_BITS)
     port map (
@@ -172,45 +187,51 @@ begin
       got => wdata_got,
       val => wdata_val);
 
-	-- The Stimuli Generator
-  Stimuli: process
- 		constant simProcessID	: T_SIM_PROCESS_ID := simRegisterProcess("Stimuli");
+	-- The Request Generator of the CPU
+  CPU_RequestGen: process
+ 		constant simProcessID	: T_SIM_PROCESS_ID := simRegisterProcess("CPU RequestGen");
 
-		-- Wait until cache and 2nd memory are ready.
-		procedure wait_cache_rdy is
+		-- no operation
+		procedure nop is
 		begin
-			wait for 1 ps; --let ready outputs settle
-			while cache_rdy /= '1' or mem2_rdy /= '1' loop
-				cache_req   <= '0';
-				cache_write <= '-';
-				cache_addr  <= (others => '-');
-				cache_wdata <= (others => '-');
+			cpu_req   <= '0';
+			cpu_write <= '-';
+			cpu_addr  <= (others => '-');
+			cpu_wdata <= (others => '-');
+			wdata_got <= '0';
+			wait until rising_edge(clk);
+		end procedure;
+
+		-- Write random data at given word address.
+		-- Waits until cache and 2nd memory are ready.
+		procedure write(addr : in natural) is
+		begin
+			-- apply request (will be ignored if not ready)
+			cpu_req   <= '1';
+			cpu_write <= '1';
+			cpu_addr  <= to_unsigned(addr, WORD_ADDR_BITS);
+			cpu_wdata <= wdata_val;
+			wdata_got <= '1';
+			while true loop
 				wait until rising_edge(clk);
-				wait for 1 ps; --let ready outputs settle
+				exit when (cache_rdy and mem2_rdy) = '1';
 			end loop;
 		end procedure;
 
-		-- Wait until cache is ready and then write random data at given address.
-		procedure write_cache (addr : in natural) is
+		-- Read at given word address.
+		-- Waits until cache and 2nd memory are ready.
+		procedure read(addr : in natural) is
 		begin
-			wait_cache_rdy;
-			cache_req   <= '1';
-			cache_write <= '1';
-			cache_addr  <= to_unsigned(addr, ADDR_BITS-BYTE_ADDR_BITS);
-			cache_wdata <= wdata_val;
-			wdata_got   <= '1';
-			wait until rising_edge(clk);
-		end procedure;
-
-		-- Wait until cache is ready and then read at given address.
-		procedure read_cache (addr : in natural) is
-		begin
-			wait_cache_rdy;
-			cache_req   <= '1';
-			cache_write <= '0';
-			cache_addr  <= to_unsigned(addr, ADDR_BITS-BYTE_ADDR_BITS);
-			cache_wdata <= (others => '-');
-			wait until rising_edge(clk);
+			-- apply request (will be ignored if not ready)
+			cpu_req   <= '1';
+			cpu_write <= '0';
+			cpu_addr  <= to_unsigned(addr, WORD_ADDR_BITS);
+			cpu_wdata <= (others => '-');
+			wdata_got <= '0';
+			while true loop
+				wait until rising_edge(clk);
+				exit when (cache_rdy and mem2_rdy) = '1';
+			end loop;
 		end procedure;
 
   begin
@@ -219,43 +240,64 @@ begin
     wait until rising_edge(clk);
     rst <= '0';
 
+		-- Check No Operation
+		-- --------------------------------------------
+		for i in 0 to 3 loop nop; end loop;
+
 		-- Fill memory with valid data and read it back
 		-- --------------------------------------------
 		-- Due to the No-Write-Allocate policy no cache hit occurs.
-		for addr in 0 to 2**ADDR_BITS-1 loop
-			write_cache(addr);
+		for addr in 0 to 2**WORD_ADDR_BITS-1 loop
+			write(addr);
 		end loop;  -- addr
-		for addr in 0 to 2**ADDR_BITS-1 loop
-			read_cache(addr);
+		for addr in 0 to 2**WORD_ADDR_BITS-1 loop
+			read(addr);
 		end loop;  -- addr
+		for i in 0 to 3 loop nop; end loop;
 
-		-- Read back again
-		-- ---------------
-		-- Cache hit occurs only if memory size equals cache size.
-		for addr in 0 to 2**ADDR_BITS-1 loop
-			read_cache(addr);
-		end loop;  -- addr
+		-- Linear access, read/write/read at every address
+		-- -----------------------------------------------
+		for addr in 0 to 2**WORD_ADDR_BITS-1 loop
+			read(addr);  -- cache hit only if cache size equals memory size.
+			write(addr); -- cache hit, write-through
+			read(addr);  -- cache hit
+			nop;
+		end loop;  -- chunk
+		for i in 0 to 3 loop nop; end loop;
+
+		-- Linear access in chunks of cache size, read/write/read every chunk
+		-- ------------------------------------------------------------------
+		for chunk in 0 to (2**WORD_ADDR_BITS / CACHE_LINES)-1 loop
+			for addr in chunk*CACHE_LINES to (chunk+1)*CACHE_LINES-1 loop
+				read(addr);  -- cache hit only if cache size equals memory size.
+			end loop; -- addr
+			for addr in chunk*CACHE_LINES to (chunk+1)*CACHE_LINES-1 loop
+				write(addr); -- cache hit, write-through
+			end loop; -- addr
+			for addr in chunk*CACHE_LINES to (chunk+1)*CACHE_LINES-1 loop
+				read(addr);  -- cache hit
+			end loop; -- addr
+			nop;
+		end loop;  -- chunk
+		for i in 0 to 3 loop nop; end loop;
 
 		-- Finished
 		-- --------
-		cache_req <= '0';
-		cache_write <= '-';
-		cache_addr  <= (others => '-');
-		cache_wdata <= (others => '-');
-		finished <= true;
+		nop;
+		finished  <= true;
 		simDeactivateProcess(simProcessID);
     wait;
-  end process Stimuli;
+  end process CPU_RequestGen;
 
-	-- The Checker
-	Checker: process
- 		constant simProcessID	: T_SIM_PROCESS_ID := simRegisterProcess("Checker");
+	-- The Checker of the CPU
+	CPU_Checker: process
+ 		constant simProcessID	: T_SIM_PROCESS_ID := simRegisterProcess("CPU Checker");
 		variable saved_rdata  : std_logic_vector(DATA_BITS-1 downto 0);
 	begin
 		-- wait until reset completes
 		wait until rising_edge(clk) and rst = '0';
 
-		-- wait until all stimuli have been applied
+		-- wait until all requests have been applied
 		while not finished loop
 			wait until rising_edge(clk);
 			simAssertion(not is_x(cache_rstb) and not is_x(mem2_rstb), "Meta-value on rstb.");
@@ -266,7 +308,8 @@ begin
 				while cache_rstb = '0' loop
 					wait until rising_edge(clk);
 					-- No new data from 2nd memory must arrive here.
-					simAssertion(mem2_rstb = '0', "Invalid reply from 2nd memory.");
+					simAssertion(not is_x(cache_rstb) and mem2_rstb = '0',
+											 "Meta-value on rstb or invalid reply from 2nd memory.");
 				end loop;
 
 				simAssertion(cache_rdata = saved_rdata, "Read data differs.");
@@ -276,6 +319,6 @@ begin
 		simDeactivateProcess(simProcessID);
 		simFinalize;
 		wait;
-	end process Checker;
+	end process CPU_Checker;
 
 end architecture sim;
