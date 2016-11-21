222b958278be752eb2b88be6d27b7339bbae7c0e
Martin Zabel <martin.zabel@tu-dresden.de>
2016-11-21 18:38:45 +0100

    Enforce usage of distributed RAM on Xilinx FPGAs also for direct-mapped cache.

--- src/cache/cache_tagunit_par.vhdl
+++ src/cache/cache_tagunit_par.vhdl
@@ -235,6 +235,22 @@ begin
 		signal TagMemory	 : T_TAG_LINE_VECTOR(CACHE_LINES-1 downto 0);
 		signal ValidMemory : std_logic_vector(CACHE_LINES-1 downto 0) := (others => '0');
 
+		-- If Address is fed from a register, then:
+		--
+		--  * the TagMemory must be implemented as distributed RAM on Xilinx
+		--    FPGAs because only this RAM has the intended mixed-port
+		--    read-during-write behavior.
+		--
+		--  * Mapping the TagMemory to block RAM is however possible on Altera
+		--    FPGAs because Quartus adds the neccessary bypass logic.
+		--
+		-- If Address is not fed from a register, then:
+		--
+		--  * distributed RAM will be used on Xilinx FPGAs anyway,
+		--  * LUTs and FFs are used on Altera FPGAs.
+		attribute ram_style              : string;  -- XST specific
+		attribute ram_style of TagMemory : signal is "distributed";
+
 		signal TagHit_i	 : std_logic;
 		signal TagMiss_i : std_logic;
 
@@ -353,7 +369,7 @@ begin
 			--  * LUTs and FFs are used on Altera FPGAs.
 			attribute ram_style              : string;  -- XST specific
 			attribute ram_style of TagMemory : signal is "distributed";
-			
+
 			signal Tag   : T_TAG_LINE; -- read tag from memory
 			signal Valid : std_logic;  -- read valid from memory
 		begin
