67267b3138c9e8efa1c4c80f8db084bff6e4d06d
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2019-07-01 00:27:40 +0200

    Added missing constants.

--- src/bus/axi4/AXI4Stream/AXI4Stream_Termination_Slave.vhdl
+++ src/bus/axi4/AXI4Stream/AXI4Stream_Termination_Slave.vhdl
@@ -49,6 +49,8 @@ end entity;
 
 
 architecture rtl of AXI4Stream_Termination_Slave is
+  constant DataBits : natural := In_S2M.Data'length;
+  constant UserBits : natural := In_S2M.User'length;
 begin
 
 	In_S2M <= Initialize_AXI4Stream_S2M(DataBits, UserBits, VALUE);
