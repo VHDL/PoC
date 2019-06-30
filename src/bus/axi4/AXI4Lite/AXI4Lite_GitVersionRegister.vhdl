36a3c457729d47778611b8db57cc2efd4425f458
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2019-06-30 23:31:51 +0200

    Minor improvements to the GitVersionRegister for AXI4Lite.

--- src/bus/axi4/AXI4Lite/AXI4Lite_GitVersionRegister.vhdl
+++ src/bus/axi4/AXI4Lite/AXI4Lite_GitVersionRegister.vhdl
@@ -1,34 +1,34 @@
-
 library IEEE;
 use     IEEE.std_logic_1164.all;
 use     IEEE.numeric_std.all;
 
-library PoC;
-use     PoC.utils.all;
-use     PoC.vectors.all;
-use     PoC.axi4.all;
-use     PoC.AXI4Lite_Version.all;
-use     PoC.strings.all;
-use     PoC.BuildVersion.all;
+use     work.utils.all;
+use     work.vectors.all;
+use     work.strings.all;
+use     work.axi4.all;
 
-entity AXI4Lite_Version_Register is
+use     work.GitVersionRegister.all;
+use     work.BuildVersion.all;
+
+
+entity AXI4Lite_GitVersionRegister is
 	Port (
 		S_AXI_ACLK              : in  std_logic;
 		S_AXI_ARESETN           : in  std_logic;
-		S_AXI_m2s               : in  T_AXI4Lite_BUS_M2S := Initialize_AXI4Lite_Bus_M2S(32, 32);
-		S_AXI_s2m               : out T_AXI4Lite_BUS_S2M := Initialize_AXI4Lite_Bus_S2M(32, 32)
+		S_AXI_m2s               : in  T_AXI4Lite_BUS_M2S;
+		S_AXI_s2m               : out T_AXI4Lite_BUS_S2M
 	);
 end entity;
 
-architecture Behavioral of AXI4Lite_Version_Register is
-	constant ADDRESS_BITS  : natural                 := 32;
-	constant DATA_BITS     : natural                 := 32;
+
+architecture rtl of AXI4Lite_GitVersionRegister is
+	constant DATA_BITS            : natural          := 32;
 	
 	constant num_Version_register : natural          := get_num_Version_register;
   
-  constant CONFIG     :   T_AXI4_Register_Description_Vector(0 to num_Version_register -1) := get_Dummy_Descriptor(num_Version_register);
+  constant CONFIG      : T_Register_Description_Vector(0 to num_Version_register -1) := get_Dummy_Descriptor(num_Version_register);
 	
-	constant VersionData           : T_SLVV_32(0 to num_Version_register -1) := (
+	constant VersionData : T_SLVV_32(0 to num_Version_register -1) := (
 			0                 to Reg_Length_Common -1                  => to_SLVV_32_Common(C_HW_BUILD_VERSION_COMMON),
 			Reg_Length_Common to Reg_Length_Common + Reg_Length_Top -1 => to_SLVV_32_Top(C_HW_BUILD_VERSION_TOP)
 		);
@@ -46,13 +46,11 @@ architecture Behavioral of AXI4Lite_Version_Register is
   constant RegisterFile_WritePort  : T_SLVV(0 to CONFIG'Length -1)(DATA_BITS -1 downto 0) := to_slvv(VersionData);
   
 begin
-	
-	
-  version_reg : entity poc.AXI4Lite_Register
-	Generic map(
+  AXI4LiteReg : entity work.AXI4Lite_Register
+	generic map(
 	 	CONFIG        => CONFIG
 	)
-	Port map(
+	port map(
 		S_AXI_ACLK              => S_AXI_ACLK             ,
 		S_AXI_ARESETN           => S_AXI_ARESETN          ,
 		S_AXI_m2s               => S_AXI_m2s              ,
@@ -60,5 +58,4 @@ begin
 		RegisterFile_ReadPort   => RegisterFile_ReadPort  ,
 		RegisterFile_WritePort  => RegisterFile_WritePort
 	);
-    
-end Behavioral;
+end architecture;
