9df68f037f99d423c05dd48f0a113e4ea2a86b0f
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2019-06-30 23:30:59 +0200

    Added file header and license. Adjusted minor coding style issues.

--- src/io/iic/iic_Passthrough.vhdl
+++ src/io/iic/iic_Passthrough.vhdl
@@ -1,15 +1,45 @@
+-- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+-- vim: tabstop=2:shiftwidth=2:noexpandtab
+-- kate: tab-width 2; replace-tabs off; indent-width 2;
+-- =============================================================================
+-- Authors:				 	Stefan Unrein
+--
+-- Entity:				 	I2C passthrough module for an FPGA with debug/sniffing outputs
+--
+-- Description:
+-- -------------------------------------
+-- This module creates a transparent I2C path through an FPGA. In addition this
+-- module offers a debug/sniffing line to log I2C operations.
+--
+--
+-- License:
+-- =============================================================================
+-- Copyright 2018-2019 PLC2 Design GmbH, Germany
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--		http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- =============================================================================
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-use IEEE.NUMERIC_STD.all;
+use     IEEE.STD_LOGIC_1164.all;
+use     IEEE.NUMERIC_STD.all;
 
-library PoC;
-use PoC.utils.all;
-use PoC.physical.all;
-use PoC.io.all;
-use PoC.iic.all;
+use     work.utils.all;
+use     work.physical.all;
+use     work.io.all;
+use     work.iic.all;
 
-entity iic_passthrough is
+
+entity iic_Passthrough is
 	generic (
 		CLOCK_FREQ     : FREQ    := 100 MHz;
 		DEBOUNCE_TIME  : T_TIME  := 50.0e-9;
@@ -18,15 +48,16 @@ entity iic_passthrough is
   port (
 		reset   : in    std_logic;
 		clock   : in    std_logic;
+		
   	port_a  : inout T_IO_IIC_SERIAL;
 		port_b  : inout T_IO_IIC_SERIAL;
+		
 		debug   : out   T_IO_IIC_SERIAL_PCB
---        stall   : out   std_logic;
---        timeout : in    std_logic_vector(31 downto 0)
 	);
 end entity;
 
-architecture rtl of iic_passthrough is
+
+architecture rtl of iic_Passthrough is
 	constant cycles  : natural := TimingToCycles(DEBOUNCE_TIME, CLOCK_FREQ);
 	constant c_data  : natural := 1;
 	constant c_clock : natural := 0;
@@ -41,17 +72,15 @@ architecture rtl of iic_passthrough is
 
   type t_state is (IDLE, ST_A, ST_B);
 
-
-
 begin
 	--SCL
-	port_a.clock.O     <= '0';
-	port_a.clock.T     <= not a_set(c_clock);
+	port_a.clock.O    <= '0';
+	port_a.clock.T    <= not a_set(c_clock);
 	
-	port_b.clock.O     <= '0';
-	port_b.clock.T     <= not b_set(c_clock);
+	port_b.clock.O    <= '0';
+	port_b.clock.T    <= not b_set(c_clock);
 	
-	debug.clock        <= debug_level(c_clock);
+	debug.clock       <= debug_level(c_clock);
 	
 	--SDA
 	port_a.data.O     <= '0';
@@ -82,10 +111,9 @@ begin
   );
 
 
-
-	fsm_gen : for i in 0 to 1 generate
-		  signal state : t_state := IDLE;
-		  signal wait_count : integer range 0 to cycles := cycles -1;
+	genFSM : for i in 0 to 1 generate
+		signal state      : t_state := IDLE;
+		signal wait_count : integer range 0 to cycles := cycles -1;
 	begin
 		debug_level(i) <= '0' when state /= IDLE else '1';
 		
@@ -137,6 +165,4 @@ begin
 			end if;
 		end process;
   end generate;
-
-
 end architecture;
