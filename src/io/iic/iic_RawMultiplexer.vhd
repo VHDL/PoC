8a66ab5d9d85cbf906b5ff6c550aa3e797e220c4
Patrick Lehmann <patrick.lehmann@plc2.de>
2019-07-26 15:37:10 +0200

    Changes due to coding style.

--- src/io/iic/iic_RawMultiplexer.vhd
+++ src/io/iic/iic_RawMultiplexer.vhd
@@ -1,32 +1,29 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use     IEEE.STD_LOGIC_1164.ALL;
+use     IEEE.NUMERIC_STD.ALL;
 
-library PoC;
-use     PoC.utils.all;
-use     PoC.io.all;
+use     work.utils.all;
+use     work.io.all;
+
 
 entity iic_RawMultiplexer is
-		generic (
-			PORTS : positive
-		);
-		port (
-			sel    :	in  unsigned(log2ceilnz(PORTS) - 1 downto 0);
-			input  :	in T_IO_TRISTATE;
+	generic (
+		PORTS : positive
+	);
+	port (
+		sel    :	in  unsigned(log2ceilnz(PORTS) - 1 downto 0);
+		input  :	in T_IO_TRISTATE;
 			
-			output : 	in T_IO_TRISTATE_VECTOR(PORTS - 1 downto 0)
-		);
-end iic_RawMultiplexer;
+		output : 	in T_IO_TRISTATE_VECTOR(PORTS - 1 downto 0)
+	);
+end entity;
 
-architecture Behavioral of iic_RawMultiplexer is
-
+architecture rtl of iic_RawMultiplexer is
 begin
+	gen: for i in 0 to PORTS - 1 generate
+		output(i).O <= input.O when sel = i else '0';
+		output(i).T <= input.T when sel = i else '0';
+	end generate;
 		
-		gen: for i in 0 to PORTS - 1 generate
-			output(i).O <= input.O when sel = i else '0';
-			output(i).T <= input.T when sel = i else '0';
-		end generate;
-		
-		input.I <= output(to_index(sel)).I;
-
-end Behavioral;
+	input.I <= output(to_index(sel)).I;
+end entity;
