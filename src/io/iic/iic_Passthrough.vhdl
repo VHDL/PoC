14e5e7d0e6bfd5fdff2eff3874f291c00d77a042
Stefan Unrein <stefan.unrein@plc2.de>
2019-11-05 08:55:33 +0100

    removed dead and unused code

--- src/io/iic/iic_Passthrough.vhdl
+++ src/io/iic/iic_Passthrough.vhdl
@@ -46,11 +46,8 @@ entity iic_Passthrough is
 		DEBUG                : boolean := false;
 		CLOCK_FREQ           : FREQ    := 100 MHz;
 		LOWEST_IIC_FREQ      : FREQ    := 70 kHz;
---		IIC_LOW_TIME         : T_TIME  := 6.4e-6;
---		SAVETY_MARGIN        : T_TIME  := 1.0e-6;
 		GLITCH_CYCLES        : natural := 8;
-		PULL_UP_CYCLES       : natural := 55;
-		ENABLE_AKTIVE_PULLUP : boolean := false
+		PULL_UP_CYCLES       : natural := 55
 	);
   port (
 		Clock                : in    std_logic;
@@ -62,8 +59,6 @@ entity iic_Passthrough is
 		Port_b_in            : in  T_IO_IIC_SERIAL_IN;
 		Port_b_out           : out T_IO_IIC_SERIAL_OUT;
 		
---		MasterIsA  : in std_logic := '1';
-		
 		Port_Debug           : out   T_IO_IIC_SERIAL_PCB
 	);
 end entity;
@@ -72,15 +67,14 @@ end entity;
 architecture rtl of iic_Passthrough is
 	ATTRIBUTE MARK_DEBUG   : string;
 	
+	constant ENABLE_AKTIVE_PULLUP : boolean := false; --This generic is currently not working correct
+	
 	constant data_pos      : natural := 1;
 	constant clock_pos     : natural := 0;
 	
 	constant BITS          : natural := 8;
 	
 	constant high_low_counter_bits : natural := log2ceilnz(integer(div(CLOCK_FREQ, LOWEST_IIC_FREQ)) * 2) +1;
-	
---	constant IIC_LOW_PER     : natural := TimingToCycles(IIC_LOW_TIME, CLOCK_FREQ);
---	constant SAVETY_CYCLES   : natural := TimingToCycles(SAVETY_MARGIN, CLOCK_FREQ);
 	
 	constant GLITCH_POS    : natural := 0;
 	constant PULL_UP_POS   : natural := 1;
@@ -101,14 +95,14 @@ architecture rtl of iic_Passthrough is
 	signal a_level_data_fe : std_logic;
 
   
-	ATTRIBUTE MARK_DEBUG of a_level_i      : SIGNAL IS "TRUE";
-	ATTRIBUTE MARK_DEBUG of b_level_i      : SIGNAL IS "TRUE";
+--	ATTRIBUTE MARK_DEBUG of a_level_i      : SIGNAL IS "TRUE";
+--	ATTRIBUTE MARK_DEBUG of b_level_i      : SIGNAL IS "TRUE";
 	ATTRIBUTE MARK_DEBUG of a_set          : SIGNAL IS "TRUE";
 	ATTRIBUTE MARK_DEBUG of b_set          : SIGNAL IS "TRUE";
-	ATTRIBUTE MARK_DEBUG of a_set_d        : SIGNAL IS "TRUE";
-	ATTRIBUTE MARK_DEBUG of b_set_d        : SIGNAL IS "TRUE";
-	ATTRIBUTE MARK_DEBUG of a_set_fe       : SIGNAL IS "TRUE";
-	ATTRIBUTE MARK_DEBUG of b_set_fe       : SIGNAL IS "TRUE";
+--	ATTRIBUTE MARK_DEBUG of a_set_d        : SIGNAL IS "TRUE";
+--	ATTRIBUTE MARK_DEBUG of b_set_d        : SIGNAL IS "TRUE";
+--	ATTRIBUTE MARK_DEBUG of a_set_fe       : SIGNAL IS "TRUE";
+--	ATTRIBUTE MARK_DEBUG of b_set_fe       : SIGNAL IS "TRUE";
 	ATTRIBUTE MARK_DEBUG of debug_level    : SIGNAL IS "TRUE";
 
 begin
@@ -163,10 +157,7 @@ begin
 		signal bit_counter       : unsigned(log2ceilnz(BITS) downto 0) := to_unsigned(BITS, log2ceilnz(BITS) +1);
 		signal is_ACK_cycle      : std_logic;
 		
---		signal high_counter_us   : unsigned(high_low_counter_bits -1 downto 0) := (others => '0');
 		signal low_counter_us    : unsigned(high_low_counter_bits -1 downto 0) := (others => '0');
-		
---		signal high_counter_us_i : unsigned(high_low_counter_bits -3 downto 0) := (others => '0');
 		signal low_counter_us_i  : unsigned(high_low_counter_bits -3 downto 0) := (others => '0');
 		
 		signal Enable				     : std_logic;																		-- enable counter
@@ -179,10 +170,9 @@ begin
 		ATTRIBUTE MARK_DEBUG of state             : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of a_level           : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of a_level_glitch    : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of a_level_glitch_d  : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of a_level_glitch_fe : SIGNAL IS "TRUE";
+--		ATTRIBUTE MARK_DEBUG of a_level_glitch_d  : SIGNAL IS "TRUE";
+--		ATTRIBUTE MARK_DEBUG of a_level_glitch_fe : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of is_frame_start    : SIGNAL IS "TRUE";
---		ATTRIBUTE MARK_DEBUG of high_counter_us_i : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of low_counter_us_i  : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of b_level           : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of bit_counter       : SIGNAL IS "TRUE";
@@ -191,19 +181,9 @@ begin
 		ATTRIBUTE MARK_DEBUG of Load              : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of Slot              : SIGNAL IS "TRUE";
 		ATTRIBUTE MARK_DEBUG of Timeout           : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of Timeout_d         : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of Timeout_re        : SIGNAL IS "TRUE";
+--		ATTRIBUTE MARK_DEBUG of Timeout_d         : SIGNAL IS "TRUE";
+--		ATTRIBUTE MARK_DEBUG of Timeout_re        : SIGNAL IS "TRUE";
 	begin
-		
-		assert DEBUG report 
---			"IIC_LOW_PER = "          & integer'image(IIC_LOW_PER) & 
---			"  , SAVETY_CYCLES = "    & integer'image(SAVETY_CYCLES) &
-			"  , high_low_counter_bits = " & integer'image(high_low_counter_bits)
---			"  , TIMING_TABLE(0) = "  & integer'image(TIMING_TABLE(0)) &
---			"  , TIMING_TABLE(1) = "  & integer'image(TIMING_TABLE(1)) &
---			"  , TIMING_TABLE(2) = "  & integer'image(TIMING_TABLE(2)) &
---			"  , TIMING_TABLE(3) = "  & integer'image(TIMING_TABLE(3))
-		severity note;
 
 		Glitch_Filter_master : entity work.io_GlitchFilter
 		generic map(
@@ -260,14 +240,10 @@ begin
 							Enable   <= '1';
 								if a_level = '1' then 
 									b_set(clock_pos) <= '0';
---									if Timeout = '1' and Load = '0' then
-										Enable         <= '0';
-										Slot           <= to_unsigned(PULL_UP_POS, 2);
-										Load           <= '1';
-										state          <= ST_AW;
---									else
---										state    <= IDLE;
---									end if;
+									Enable           <= '0';
+									Slot             <= to_unsigned(PULL_UP_POS, 2);
+									Load             <= '1';
+									state            <= ST_AW;
 								elsif is_ACK_cycle = '1' then
 									Slot             <= to_unsigned(ACK_WAIT_POS, 2);
 									Load             <= '1';
@@ -287,16 +263,12 @@ begin
 						
 						when ST_ACKW =>
 							Enable           <= '1';
---							if a_level = '0' then
---								b_set(clock_pos)   <= '1';
---							end if;
 							b_set(clock_pos) <= '1';
 							if is_ACK_cycle = '0' then
 								Slot           <= to_unsigned(GLITCH_POS, 2);
 								Load           <= '1';
 								state          <= ST_A;
 							elsif Timeout_re = '1' then
---								b_set(clock_pos)   <= '1';
 								Slot           <= to_unsigned(ACK_PULL_POS, 2);
 								Load           <= '1';
 								state          <= ST_ACK;
@@ -325,14 +297,10 @@ begin
 							Enable   <= '1';
 								if b_level = '1' then 
 									a_set(clock_pos) <= '0';
---									if Timeout = '1' and Load = '0' then
 										Enable         <= '0';
 										Slot           <= to_unsigned(PULL_UP_POS, 2);
 										Load           <= '1';
 										state          <= ST_BW;
---									else
---										state    <= IDLE;
---									end if;
 								end if;
 								
 						when ST_BW => 
@@ -364,8 +332,8 @@ begin
 		
 			signal Counter_s				: signed(COUNTER_BITS downto 0)		:= to_signed(glitch_cycles -1, COUNTER_BITS + 1);
 			
-			ATTRIBUTE MARK_DEBUG of TIMING_TABLE     : SIGNAL IS "TRUE";
-			ATTRIBUTE MARK_DEBUG of savety           : SIGNAL IS "TRUE";
+--			ATTRIBUTE MARK_DEBUG of TIMING_TABLE     : SIGNAL IS "TRUE";
+--			ATTRIBUTE MARK_DEBUG of savety           : SIGNAL IS "TRUE";
 			ATTRIBUTE MARK_DEBUG of Counter_s        : SIGNAL IS "TRUE";
 		begin	
 			TIMING_TABLE(GLITCH_POS)   <= std_logic_vector(to_signed(glitch_cycles, COUNTER_BITS +1));
@@ -387,18 +355,6 @@ begin
 			end process;
 
 			Timeout <= Counter_s(Counter_s'high);
-		
---		counter : entity work.io_TimingCounter
---		generic map(
---			TIMING_TABLE	=> TIMING_TABLE
---		)
---		port map(
---			Clock					=> clock,
---			Enable				=> Enable,
---			Load					=> Load,
---			Slot					=> Slot,
---			Timeout				=> Timeout
---		);
 		end block;
 		
 		is_ACK_cycle <= to_sl(bit_counter = BITS);
@@ -414,13 +370,10 @@ begin
 					else
 						bit_counter <= (others => '0');
 					end if;
---				elsif is_frame_start = '1' then
---					bit_counter <= to_unsigned(BITS, bit_counter'length);
 				end if;
 			end if;
 		end process;
 		
---		high_counter_us_i <= high_counter_us(high_counter_us'high downto 2);
 		low_counter_us_i  <= low_counter_us(low_counter_us'high downto 2);
 		
 		high_low_counter_proc : process(clock)
@@ -428,14 +381,10 @@ begin
 			if rising_edge(clock) then
 				if bit_counter = 0 then
 					low_counter_us  <= (others => '0');
---					high_counter_us <= (others => '0');
 				elsif (bit_counter < BITS) and (bit_counter > 3) then
 					if a_level_glitch = '0' then
 						low_counter_us <= low_counter_us +1;
 					end if;
---					if a_level_glitch = '1' then
---						high_counter_us <= high_counter_us +1;
---					end if;
 				end if;
 			end if;
 		end process;
@@ -450,7 +399,7 @@ begin
   	type t_state is (IDLE, ST_A, ST_B, ST_BW, ST_AW);	
 		
 		signal state      : t_state := IDLE;
---		signal wait_count : integer range 0 to cycles := cycles;
+		
 		signal a_level         : std_logic;
 		signal b_level         : std_logic;
 		
@@ -465,12 +414,12 @@ begin
 		signal Timeout_re  	: std_logic;																			-- timing reached
 		
 		ATTRIBUTE MARK_DEBUG of state    : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of a_level    : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of b_level    : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of Enable    : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of Load    : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of Slot    : SIGNAL IS "TRUE";
-		ATTRIBUTE MARK_DEBUG of Timeout    : SIGNAL IS "TRUE";
+		ATTRIBUTE MARK_DEBUG of a_level  : SIGNAL IS "TRUE";
+		ATTRIBUTE MARK_DEBUG of b_level  : SIGNAL IS "TRUE";
+		ATTRIBUTE MARK_DEBUG of Enable   : SIGNAL IS "TRUE";
+		ATTRIBUTE MARK_DEBUG of Load     : SIGNAL IS "TRUE";
+		ATTRIBUTE MARK_DEBUG of Slot     : SIGNAL IS "TRUE";
+		ATTRIBUTE MARK_DEBUG of Timeout  : SIGNAL IS "TRUE";
 	begin
 		Glitch_Filter_master_data : entity work.io_GlitchFilter
 		generic map(
@@ -498,77 +447,69 @@ begin
 			if rising_edge(clock) then
 				a_set(data_pos) <= '0';
 				b_set(data_pos) <= '0';
-				Slot     <= GLITCH_POS;
-				Enable   <= '0';
-				Load     <= '0';
+				Slot            <= GLITCH_POS;
+				Enable          <= '0';
+				Load            <= '0';
 
 				if reset = '1' then
 					state      <= IDLE;
 				else
 					case state is
 						when IDLE => 
-							Slot     <= GLITCH_POS;
+							Slot              <= GLITCH_POS;
 							if a_level = '0' then
-								Load       <= '1';
-								state      <= ST_A;
-								b_set(data_pos)   <= '1';
+								Load            <= '1';
+								state           <= ST_A;
+								b_set(data_pos) <= '1';
 							end if;
 							if b_level = '0' then 
-								Load     <= '1';
-								state    <= ST_B;
+								Load            <= '1';
+								state           <= ST_B;
 								a_set(data_pos) <= '1';
 							end if;
 
 						when ST_A => 
-							b_set(data_pos) <= '1';
-							Enable   <= '1';
-								if a_level = '1' then 
-									b_set(data_pos) <= '0';
---									if Timeout = '1' and Load = '0' then
-										Enable   <= '0';
-										Slot     <= PULL_UP_POS;
-										Load     <= '1';
-										state    <= ST_AW;
---									else
---										state    <= IDLE;
---									end if;
-								end if;
+							b_set(data_pos)   <= '1';
+							Enable            <= '1';
+							if a_level = '1' then 
+								b_set(data_pos) <= '0';
+								Enable          <= '0';
+								Slot            <= PULL_UP_POS;
+								Load            <= '1';
+								state           <= ST_AW;
+							end if;
 								
 						when ST_AW => 
-							Enable   <= '1';
+							Enable            <= '1';
 							if a_level = '0' then 
-								Slot     <= GLITCH_POS;
-								Load       <= '1';
-								state      <= ST_A;
-								b_set(data_pos)   <= '1';
+								Slot            <= GLITCH_POS;
+								Load            <= '1';
+								state           <= ST_A;
+								b_set(data_pos) <= '1';
 							elsif Timeout_re = '1' then
-								state      <= IDLE;
+								state           <= IDLE;
 							end if;							
 
 						when ST_B => 
-							a_set(data_pos) <= '1';
-							Enable   <= '1';
-								if b_level = '1' then 
-									a_set(data_pos) <= '0';
---									if Timeout = '1' and Load = '0' then
-										Enable   <= '0';
-										Slot     <= PULL_UP_POS;
-										Load     <= '1';
-										state    <= ST_BW;
---									else
---										state    <= IDLE;
---									end if;
-								end if;
+							a_set(data_pos)   <= '1';
+							Enable            <= '1';
+							if b_level = '1' then 
+								a_set(data_pos) <= '0';
+								Enable          <= '0';
+								Slot            <= PULL_UP_POS;
+								Load            <= '1';
+								state           <= ST_BW;
+							end if;
 								
 						when ST_BW => 
-							Enable   <= '1';
+							Enable            <= '1';
 							if b_level = '0' then 
-								Slot     <= GLITCH_POS;
-								Load       <= '1';
-								state      <= ST_B;
-								a_set(data_pos)   <= '1';
+								Slot            <= GLITCH_POS;
+								Load            <= '1';
+								state           <= ST_B;
+								a_set(data_pos) <= '1';
 							elsif Timeout_re = '1' then
-								state      <= IDLE;
+								state           <= IDLE;
 							end if;					
 					end case;
 				end if;
