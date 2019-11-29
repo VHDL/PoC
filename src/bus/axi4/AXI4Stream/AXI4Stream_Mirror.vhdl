ee41d9d853179f7564cb42ba10c40ea42590f68f
Max <max.kraft-kugler@plc2.de>
2019-11-29 16:18:26 +0100

    removed duplicate bracket

--- src/bus/axi4/AXI4Stream/AXI4Stream_Mirror.vhdl
+++ src/bus/axi4/AXI4Stream/AXI4Stream_Mirror.vhdl
@@ -161,7 +161,7 @@ begin
 	gen_lost:for i in 0 to PORTS - 1 generate
 		-- transaction is considered lost when:
 		-- the mirror transaction (all ports are either ready or masked) has happend but current slave had no transaction (valid still asserted without a ready)
-		mask_transaction_lost(i) <= ((glue_got and (Out_M2S(i).Valid and not Out_S2M(i).Ready));
+		mask_transaction_lost(i) <= (glue_got and (Out_M2S(i).Valid and not Out_S2M(i).Ready));
 	end generate;
 
 end architecture;
