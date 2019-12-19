5ad6562a15ed1840b5d7f29d0cbd3302fa42e5c0
Max <max.kraft-kugler@plc2.de>
2019-12-19 10:38:55 +0100

    also for auto boundary

--- src/misc/stat/stat_Histogram_auto_boundary.vhdl
+++ src/misc/stat/stat_Histogram_auto_boundary.vhdl
@@ -102,12 +102,12 @@ begin
 			if (bucket_n = NUM_OF_BUCKETS - 1) then
 				--check if datum is above highest bucket's threshhold
 				if DataIn >= ((bucket_n*window_steps) + window_bounds_lower) then
-					buckets <= bucket_n;
+					buckets <= std_logic_vector(to_unsigned(bucket_n, buckets'length));
 				end if;
 			else
 				--check if datum is between this and next bucket's threshhold
 				if (DataIn >= ((bucket_n*window_steps) + window_bounds_lower)) and (DataIn < (((bucket_n + 1 )*window_steps) + window_bounds_lower)) then
-					buckets <= bucket_n;
+					buckets <= std_logic_vector(to_unsigned(bucket_n, buckets'length));
 				end if;
 			end if;
 		end loop;
