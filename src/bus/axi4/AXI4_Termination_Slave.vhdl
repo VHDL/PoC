eea2b4d227dafef09937acf076eccca735181a53
Max <max.kraft-kugler@plc2.de>
2020-01-21 12:04:29 +0100

    simplified full slave termination and made it always be ready and respond with an error to ensure bus is never blocked by this module

--- src/bus/axi4/AXI4_Termination_Slave.vhdl
+++ src/bus/axi4/AXI4_Termination_Slave.vhdl
@@ -2,10 +2,11 @@
 -- vim: tabstop=2:shiftwidth=2:noexpandtab
 -- kate: tab-width 2; replace-tabs off; indent-width 2;
 -- =============================================================================
--- Authors:				 	Stefan Unrein
+-- Authors:         Stefan Unrein
 --                  Patrick Lehmann
+--                  Max Kraft-Kugler
 --
--- Entity:				 	A slave-side bus termination module for AXI4 (full).
+-- Entity:          A slave-side bus termination module for AXI4 (full).
 --
 -- Description:
 -- -------------------------------------
@@ -20,7 +21,7 @@
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
 --
---		http://www.apache.org/licenses/LICENSE-2.0
+--    http://www.apache.org/licenses/LICENSE-2.0
 --
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,9 +37,6 @@ use     work.axi4.all;
 
 
 entity AXI4_Termination_Slave is
-	generic (
-		VALUE     : std_logic := '0'
-	);
 	port ( 
 		AXI4_M2S  : in  T_AXI4_Bus_M2S;
 		AXI4_S2M  : out T_AXI4_Bus_S2M
@@ -47,14 +45,18 @@ end entity;
 
 
 architecture rtl of AXI4_Termination_Slave is
-	constant AddrBits : natural := AXI4_M2S.AWAddr'length;
-	constant IDBits   : natural := AXI4_M2S.AWID'length;
-	constant UserBits : natural := AXI4_M2S.AWUser'length;
-	constant DataBits : natural := AXI4_M2S.WData'length;
-	signal M2S_dummy : AXI4_M2S'subtype;
 begin
-
-	AXI4_S2M <= Initialize_AXI4_Bus_S2M(AddrBits, DataBits, UserBits, IDBits, VALUE);
-	M2S_dummy <= AXI4_M2S;
-	
+	AXI4_S2M.AWReady <= '1';
+	AXI4_S2M.WReady  <= '1';
+	AXI4_S2M.BValid  <= '1';
+	AXI4_S2M.BResp   <= C_AXI4_RESPONSE_SLAVE_ERROR;
+	AXI4_S2M.BID     <= (others => '0');
+	AXI4_S2M.BUser   <= (others => '0');
+	AXI4_S2M.ARReady <= '1';
+	AXI4_S2M.RValid  <= '1';
+	AXI4_S2M.RData   <= (DataBits - 1 downto 0 => Value);
+	AXI4_S2M.RResp   <= C_AXI4_RESPONSE_SLAVE_ERROR;
+	AXI4_S2M.RID     <= (others => '0');
+	AXI4_S2M.RLast   <= '0';
+	AXI4_S2M.RUser   <= (others => '0');
 end architecture;
