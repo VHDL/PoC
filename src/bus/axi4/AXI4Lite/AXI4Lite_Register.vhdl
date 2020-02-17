512ba24f4c2cb6be61bface8597f811e637889ad
Stefan Unrein <stefan.unrein@plc2.de>
2020-02-17 18:02:41 +0100

    set read address to '0' on reset

--- src/bus/axi4/AXI4Lite/AXI4Lite_Register.vhdl
+++ src/bus/axi4/AXI4Lite/AXI4Lite_Register.vhdl
@@ -295,7 +295,7 @@ begin
 		if rising_edge(S_AXI_ACLK) then 
 			if (S_AXI_ARESETN = '0') then
 				axi_arready <= '0';
-				axi_araddr  <= (others => '1');
+				axi_araddr  <= (others => '0');
 			elsif (axi_arready = '0' and S_AXI_m2s.ARValid = '1' and outstanding_read = '0') then
 				axi_arready <= '1';
 				axi_araddr  <= S_AXI_m2s.ARAddr(S_AXI_m2s.ARAddr'high downto ADDR_LSB);
@@ -376,8 +376,7 @@ begin
 		is_high_r <= '1' when axi_araddr(axi_araddr'high downto REG_ADDRESS_BITS - ADDR_LSB) = (axi_araddr'high downto REG_ADDRESS_BITS - ADDR_LSB => '0') else '0';
 		is_high_w <= '1' when axi_awaddr(axi_awaddr'high downto REG_ADDRESS_BITS - ADDR_LSB) = (axi_awaddr'high downto REG_ADDRESS_BITS - ADDR_LSB => '0') else '0';
 	end generate;
-	
-	
+
 	hit_gen_r : for i in hit_r'range generate
 		signal config_addr  : unsigned(REG_ADDRESS_BITS - 1 downto ADDR_LSB);
 		signal is_config    : std_logic;
@@ -386,10 +385,7 @@ begin
 		is_config    <= '1' when std_logic_vector(config_addr) = axi_araddr(REG_ADDRESS_BITS - ADDR_LSB -1 downto 0) else '0';
 		hit_r(i)     <= '1' when (is_config = '1') and (is_high_r = '1') else '0';
 	end generate;
-	
-	
-	
-	
+
 	hit_gen_w : for i in hit_w'range generate
 		signal config_addr : unsigned(REG_ADDRESS_BITS - 1 downto ADDR_LSB);
 		signal is_config    : std_logic;
