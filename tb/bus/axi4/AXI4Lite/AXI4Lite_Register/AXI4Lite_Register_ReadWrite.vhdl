32c5941b7f630fcb30211f82974eb6b7e0b261d9
Adrian <adrian.weiland@plc2.de>
2025-06-27 18:04:38 +0200

    fix header

--- tb/bus/axi4/AXI4Lite/AXI4Lite_Register/AXI4Lite_Register_ReadWrite.vhdl
+++ tb/bus/axi4/AXI4Lite/AXI4Lite_Register/AXI4Lite_Register_ReadWrite.vhdl
@@ -1,12 +1,31 @@
+-- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+-- vim: tabstop=2:shiftwidth=2:noexpandtab
+-- kate: tab-width 2; replace-tabs off; indent-width 2;
 -- =============================================================================
 -- Authors:
--- Adrian Weiland
+--                  Adrian Weiland (PLC2 Design GmbH)
+--
+-- Entity:          ReadWrite
+--
+-- Description:
+-- -------------------------------------
+-- Test-Case ReadWrite
 --
 -- License:
 -- =============================================================================
--- Copyright (c) 2024 PLC2 Design GmbH - All Rights Reserved
--- Unauthorized copying of this file, via any medium is strictly prohibited.
--- Proprietary and confidential
+-- Copyright 2025-2025 The PoC-Library Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--        http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS of ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 -- =============================================================================
 
 architecture ReadWrite of AXI4Lite_Register_TestController is
