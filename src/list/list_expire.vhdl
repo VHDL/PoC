7de28103e669cc39b9d62778f1dd8325312e40db
Stefan Unrein <stefan_unrein@gmx.de>
2025-08-27 14:01:00 +0200

    fix lincense header

--- src/list/list_expire.vhdl
+++ src/list/list_expire.vhdl
@@ -1,126 +1,136 @@
 -- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
 -- vim: tabstop=2:shiftwidth=2:noexpandtab
 -- kate: tab-width 2; replace-tabs off; indent-width 2;
--- 
--- ============================================================================
--- Module:				 	TODO
 --
--- Authors:				 	Patrick Lehmann
--- 
+-- ============================================================================
+-- Module:          list_expire
+--
+-- Authors:         Patrick Lehmann
+--
 -- Description:
 -- ------------------------------------
---		TODO
+--    TODO
 --
 -- License:
 -- =============================================================================
--- Copyright (c) 2024 PLC2 Design GmbH - All Rights Reserved
--- Unauthorized copying of this file, via any medium is strictly prohibited.
--- Proprietary and confidential
+-- Copyright 2024-2025 The PoC-Library Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS of ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 -- =============================================================================
 
 LIBRARY IEEE;
-USE			IEEE.STD_LOGIC_1164.ALL;
-USE			IEEE.NUMERIC_STD.ALL;
+USE     IEEE.STD_LOGIC_1164.ALL;
+USE     IEEE.NUMERIC_STD.ALL;
 
 
-use work.utils.ALL;
+use     work.utils.ALL;
 
 -- list_expire_fixed
---		expire	= list of expireable items
---		fixed		= insert_time := current_time + fixed interval
+--    expire  = list of expireable items
+--    fixed    = insert_time := current_time + fixed interval
 
 ENTITY list_expire IS
 	GENERIC (
-		CLOCK_CYCLE_TICKS					: POSITIVE												:= 1024;
-		EXPIRATION_TIME_TICKS			: NATURAL													:= 10;
-		ELEMENTS									: POSITIVE												:= 32;
-		KEY_BITS									: POSITIVE												:= 4
+		CLOCK_CYCLE_TICKS         : POSITIVE   := 1024;
+		EXPIRATION_TIME_TICKS     : NATURAL    := 10;
+		ELEMENTS                  : POSITIVE   := 32;
+		KEY_BITS                  : POSITIVE   := 4
 	);
 	PORT (
-		Clock											: IN	STD_LOGIC;
-		Reset											: IN	STD_LOGIC;
-		
-		Tick											: IN	STD_LOGIC;
-		
-		Insert										: IN	STD_LOGIC;
-		KeyIn											: IN	STD_LOGIC_VECTOR(KEY_BITS - 1 DOWNTO 0);
-		
-		Expired										: OUT	STD_LOGIC;
-		KeyOut										: OUT	STD_LOGIC_VECTOR(KEY_BITS - 1 DOWNTO 0)
+		Clock                     : IN  STD_LOGIC;
+		Reset                     : IN  STD_LOGIC;
+
+		Tick                      : IN  STD_LOGIC;
+
+		Insert                    : IN  STD_LOGIC;
+		KeyIn                     : IN  STD_LOGIC_VECTOR(KEY_BITS - 1 DOWNTO 0);
+
+		Expired                   : OUT  STD_LOGIC;
+		KeyOut                    : OUT  STD_LOGIC_VECTOR(KEY_BITS - 1 DOWNTO 0)
 	);
-END;
+END ENTITY;
 
 
 ARCHITECTURE rtl OF list_expire IS
-	ATTRIBUTE KEEP										: BOOLEAN;
+	ATTRIBUTE KEEP                    : BOOLEAN;
 
-	CONSTANT CLOCK_BITS								: POSITIVE																								:= log2ceilnz(CLOCK_CYCLE_TICKS);
-	
-	SIGNAL CurrentTime_us							: UNSIGNED(CLOCK_BITS - 1 DOWNTO 0)												:= (OTHERS => '0');
-	SIGNAL KeyTime_us									: UNSIGNED(CLOCK_BITS + KEY_BITS - 1 DOWNTO KEY_BITS);
-	
-	SIGNAL FIFO_put										: STD_LOGIC;
-	SIGNAL FIFO_DataIn								: STD_LOGIC_VECTOR(CLOCK_BITS + KEY_BITS - 1 DOWNTO 0);
-	SIGNAL FIFO_Full									: STD_LOGIC;
-	SIGNAL FIFO_got										: STD_LOGIC;
-	SIGNAL FIFO_DataOut								: STD_LOGIC_VECTOR(CLOCK_BITS + KEY_BITS - 1 DOWNTO 0);
-	SIGNAL FIFO_Valid									: STD_LOGIC;
-	
-	SIGNAL Expired_i									: STD_LOGIC;
-	
+	CONSTANT CLOCK_BITS               : POSITIVE                                             := log2ceilnz(CLOCK_CYCLE_TICKS);
+
+	SIGNAL CurrentTime_us             : UNSIGNED(CLOCK_BITS - 1 DOWNTO 0)                    := (OTHERS => '0');
+	SIGNAL KeyTime_us                 : UNSIGNED(CLOCK_BITS + KEY_BITS - 1 DOWNTO KEY_BITS);
+
+	SIGNAL FIFO_put                   : STD_LOGIC;
+	SIGNAL FIFO_DataIn                : STD_LOGIC_VECTOR(CLOCK_BITS + KEY_BITS - 1 DOWNTO 0);
+	SIGNAL FIFO_Full                  : STD_LOGIC;
+	SIGNAL FIFO_got                   : STD_LOGIC;
+	SIGNAL FIFO_DataOut               : STD_LOGIC_VECTOR(CLOCK_BITS + KEY_BITS - 1 DOWNTO 0);
+	SIGNAL FIFO_Valid                 : STD_LOGIC;
+
+	SIGNAL Expired_i                  : STD_LOGIC;
+
 BEGIN
 
 	PROCESS(Clock)
 	BEGIN
 		IF rising_edge(Clock) THEN
 			IF (Reset = '1') THEN
-				CurrentTime_us	<= (OTHERS => '0');
+				CurrentTime_us  <= (OTHERS => '0');
 			ELSE
 				IF (Tick = '1') THEN
-					CurrentTime_us	<= CurrentTime_us + 1;
+					CurrentTime_us  <= CurrentTime_us + 1;
 				END IF;
 			END IF;
 		END IF;
 	END PROCESS;
 
-	KeyTime_us											<= CurrentTime_us + EXPIRATION_TIME_TICKS;
+	KeyTime_us                      <= CurrentTime_us + EXPIRATION_TIME_TICKS;
 
-	FIFO_put												<= Insert;
-	FIFO_DataIn(KeyIn'range)				<= KeyIn;
-	FIFO_DataIn(KeyTime_us'range)		<= std_logic_vector(KeyTime_us);
+	FIFO_put                        <= Insert;
+	FIFO_DataIn(KeyIn'range)        <= KeyIn;
+	FIFO_DataIn(KeyTime_us'range)   <= std_logic_vector(KeyTime_us);
 
 	FIFO : entity work.fifo_cc_got
 		GENERIC MAP (
-			D_BITS							=> CLOCK_BITS + KEY_BITS,		-- Data Width
-			MIN_DEPTH						=> ELEMENTS,								-- Minimum FIFO Depth
-			DATA_REG						=> TRUE,										-- Store Data Content in Registers
-			STATE_REG						=> TRUE,										-- Registered Full/Empty Indicators
-			OUTPUT_REG					=> FALSE,										-- Registered FIFO Output
-			ESTATE_WR_BITS			=> 0,												-- Empty State Bits
-			FSTATE_RD_BITS			=> 0												-- Full State Bits
+			D_BITS              => CLOCK_BITS + KEY_BITS,   -- Data Width
+			MIN_DEPTH           => ELEMENTS,                -- Minimum FIFO Depth
+			DATA_REG            => TRUE,                    -- Store Data Content in Registers
+			STATE_REG           => TRUE,                    -- Registered Full/Empty Indicators
+			OUTPUT_REG          => FALSE,                   -- Registered FIFO Output
+			ESTATE_WR_BITS      => 0,                       -- Empty State Bits
+			FSTATE_RD_BITS      => 0                        -- Full State Bits
 		)
 		PORT MAP (
 			-- Global Reset and Clock
-			clk									=> Clock,
-			rst									=> Reset,
-			
+			clk                 => Clock,
+			rst                 => Reset,
+
 			-- Writing Interface
-			put									=> FIFO_put,
-			din									=> FIFO_DataIn,
-			full								=> OPEN,--FIFO_Full,
-			estate_wr						=> OPEN,
+			put                 => FIFO_put,
+			din                 => FIFO_DataIn,
+			full                => OPEN,--FIFO_Full,
+			estate_wr           => OPEN,
 
 			-- Reading Interface
-			got									=> FIFO_got,
-			dout								=> FIFO_DataOut,
-			valid								=> FIFO_Valid,
-			fstate_rd						=> OPEN
+			got                 => FIFO_got,
+			dout                => FIFO_DataOut,
+			valid               => FIFO_Valid,
+			fstate_rd           => OPEN
 		);
 
-	FIFO_got			<= Expired_i;
+	FIFO_got      <= Expired_i;
 
-	Expired_i			<= to_sl(FIFO_DataOut(KeyTime_us'range) = std_logic_vector(CurrentTime_us)) AND FIFO_Valid;
+	Expired_i     <= to_sl(FIFO_DataOut(KeyTime_us'range) = std_logic_vector(CurrentTime_us)) AND FIFO_Valid;
 
-	Expired				<= Expired_i;
-	KeyOut				<= FIFO_DataOut(KeyIn'range);
+	Expired       <= Expired_i;
+	KeyOut        <= FIFO_DataOut(KeyIn'range);
 END ARCHITECTURE;
--- src/misc/misc_Sequencer.vhdl
+++ src/misc/misc_Sequencer.vhdl
@@ -1,67 +1,74 @@
 -- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
 -- vim: tabstop=2:shiftwidth=2:noexpandtab
 -- kate: tab-width 2; replace-tabs off; indent-width 2;
--- 
--- ============================================================================
--- Module:				 	TODO
 --
--- Authors:				 	Patrick Lehmann
--- 
+-- ============================================================================
+-- Module:           TODO
+--
+-- Authors:          Patrick Lehmann
+--
 -- Description:
 -- ------------------------------------
---		TODO
+--    TODO
 --
 -- License:
 -- =============================================================================
--- Copyright (c) 2024 PLC2 Design GmbH - All Rights Reserved
--- Unauthorized copying of this file, via any medium is strictly prohibited.
--- Proprietary and confidential
+-- Copyright 2024-2025 The PoC-Library Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS of ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 -- =============================================================================
 
 LIBRARY IEEE;
-USE			IEEE.STD_LOGIC_1164.ALL;
-USE			IEEE.NUMERIC_STD.ALL;
+USE     IEEE.STD_LOGIC_1164.ALL;
+USE     IEEE.NUMERIC_STD.ALL;
 
-
-use work.utils.ALL;
---use work.vectors.ALL;
-
+use     work.utils.ALL;
 
 ENTITY misc_Sequencer IS
-  GENERIC (
-	  INPUT_BITS					: POSITIVE					:= 32;
-		OUTPUT_BITS					: POSITIVE					:= 8;
-		REGISTERED					: BOOLEAN						:= FALSE
+	GENERIC (
+		INPUT_BITS          : POSITIVE          := 32;
+		OUTPUT_BITS         : POSITIVE          := 8;
+		REGISTERED          : BOOLEAN           := FALSE
 	);
-  PORT (
-	  Clock								: IN	STD_LOGIC;
-		Reset								: IN	STD_LOGIC;
-		
-		Input								: IN	STD_LOGIC_VECTOR(INPUT_BITS - 1 DOWNTO 0);
-		rst									:	IN	STD_LOGIC;
-		rev									:	IN	STD_LOGIC;
-		nxt									:	IN	STD_LOGIC;
-		Output							: OUT STD_LOGIC_VECTOR(OUTPUT_BITS - 1 DOWNTO 0)
+	PORT (
+		Clock               : IN  STD_LOGIC;
+		Reset               : IN  STD_LOGIC;
+
+		Input               : IN  STD_LOGIC_VECTOR(INPUT_BITS - 1 DOWNTO 0);
+		rst                 : IN  STD_LOGIC;
+		rev                 : IN  STD_LOGIC;
+		nxt                 : IN  STD_LOGIC;
+		Output              : OUT STD_LOGIC_VECTOR(OUTPUT_BITS - 1 DOWNTO 0)
 	);
 END;
 
 
 ARCHITECTURE rtl OF misc_Sequencer IS
-	CONSTANT CHUNKS				: POSITIVE := div_ceil(INPUT_BITS, OUTPUT_BITS);
-	CONSTANT COUNTER_BITS	: POSITIVE := log2ceilnz(CHUNKS);
+	CONSTANT CHUNKS        : POSITIVE := div_ceil(INPUT_BITS, OUTPUT_BITS);
+	CONSTANT COUNTER_BITS  : POSITIVE := log2ceilnz(CHUNKS);
 
-	SUBTYPE T_CHUNK				IS STD_LOGIC_VECTOR(OUTPUT_BITS - 1 DOWNTO 0);
-	TYPE		T_MUX					IS ARRAY (NATURAL RANGE <>) OF T_CHUNK;
-	
-	SIGNAL Mux_Data				: T_MUX(CHUNKS - 1 DOWNTO 0);
-	SIGNAL Mux_Data_d			: T_MUX(CHUNKS - 1 DOWNTO 0);
-	SIGNAL Mux_sel_us			: UNSIGNED(COUNTER_BITS - 1 DOWNTO 0)		:= (OTHERS => '0');
-	
-	SIGNAL rev_l					: STD_LOGIC															:= '0';
-	
+	SUBTYPE T_CHUNK        IS STD_LOGIC_VECTOR(OUTPUT_BITS - 1 DOWNTO 0);
+	TYPE    T_MUX          IS ARRAY (NATURAL RANGE <>) OF T_CHUNK;
+
+	SIGNAL Mux_Data       : T_MUX(CHUNKS - 1 DOWNTO 0);
+	SIGNAL Mux_Data_d     : T_MUX(CHUNKS - 1 DOWNTO 0);
+	SIGNAL Mux_sel_us     : UNSIGNED(COUNTER_BITS - 1 DOWNTO 0)    := (OTHERS => '0');
+
+	SIGNAL rev_l          : STD_LOGIC                              := '0';
+
 BEGIN
 	genMuxData : FOR I IN 0 TO CHUNKS - 1 GENERATE
-		Mux_Data(I)		<= Input(((I + 1) * OUTPUT_BITS) - 1 DOWNTO I * OUTPUT_BITS);
+		Mux_Data(I)    <= Input(((I + 1) * OUTPUT_BITS) - 1 DOWNTO I * OUTPUT_BITS);
 	END GENERATE;
 
 	genRegistered0 : IF (REGISTERED = TRUE) GENERATE
@@ -69,39 +76,39 @@ BEGIN
 		BEGIN
 			IF rising_edge(Clock) THEN
 				IF (Reset = '1') THEN
-					Mux_Data_d		<= (OTHERS => (OTHERS => '0'));
+					Mux_Data_d    <= (OTHERS => (OTHERS => '0'));
 				ELSE
-					Mux_Data_d		<= Mux_Data;
+					Mux_Data_d    <= Mux_Data;
 				END IF;
 			END IF;
 		END PROCESS;
 	END GENERATE;
 	genRegistered1 : IF (REGISTERED = FALSE) GENERATE
-		Mux_Data_d		<= Mux_Data;
+		Mux_Data_d    <= Mux_Data;
 	END GENERATE;
-	
+
 	PROCESS(Clock)
 	BEGIN
 		IF rising_edge(Clock) THEN
 			IF ((Reset OR rst) = '1') THEN
-				rev_l							<= rev;
-			
+				rev_l             <= rev;
+
 				IF (rev = '0') THEN
-					Mux_sel_us			<= to_unsigned(0,							Mux_sel_us'length);
+					Mux_sel_us      <= to_unsigned(0,             Mux_sel_us'length);
 				ELSE
-					Mux_sel_us			<= to_unsigned((CHUNKS - 1),	Mux_sel_us'length);
+					Mux_sel_us      <= to_unsigned((CHUNKS - 1),  Mux_sel_us'length);
 				END IF;
 			ELSE
 				IF (nxt = '1') THEN
 					IF (rev_l = '0') THEN
-						Mux_sel_us		<= Mux_sel_us + 1;
+						Mux_sel_us    <= Mux_sel_us + 1;
 					ELSE
-						Mux_sel_us		<= Mux_sel_us - 1;
+						Mux_sel_us    <= Mux_sel_us - 1;
 					END IF;
 				END IF;
 			END IF;
 		END IF;
 	END PROCESS;
-	
-	Output		<= Mux_Data_d(ite((SIMULATION = TRUE), imin(to_integer(Mux_sel_us), CHUNKS - 1), to_integer(Mux_sel_us)));
-END;
+
+	Output    <= Mux_Data_d(ite((SIMULATION = TRUE), imin(to_integer(Mux_sel_us), CHUNKS - 1), to_integer(Mux_sel_us)));
+END ARCHITECTURE;
